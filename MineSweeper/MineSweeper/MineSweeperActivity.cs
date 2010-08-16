8420274cc4dd940ec17b7a6265c95eb87fe1ec7c
Martin Baulig <martin@novell.com>
2010-08-16 18:17:14 +0200

    Add my samples.

---
+++ MineSweeper/MineSweeper/MineSweeperActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Graphics;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+using Android.OS;
+
+namespace Novell.DroidSamples.MineSweeper
+{
+	public class MineSweeperActivity : Activity
+	{
+		public MineSweeperActivity (IntPtr handle)
+			: base (handle)
+		{ }
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (R.layout.mine_sweeper);
+		}
+	}
+}
+
---
+++ MineSweeper/MineSweeper/R.cs
@@ -0,0 +1,3 @@
+public partial class R
+{
+}
