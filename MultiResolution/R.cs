c5e4fb14702ee017fc0424cd09c8f34c6dbbf6c9
Jonathan Pryor <jonpryor@vt.edu>
2010-08-17 15:08:51 -0400

    Set <RootNamespace/> to an actually used namespace.
    Recent versions of Novell.MonoDroid.Build.Tasks.dll place the generated
    R type into the <RootNamespace/> namespace, so if this namespace isn't
    actually used by the program we get compiler errors.

--- MultiResolution/R.cs
+++ MultiResolution/R.cs
@@ -1,103 +1,108 @@
-public partial class R
+namespace Mono.Samples.MultiResolution
 {
 	
-	public class attr
-	{
-		
-		private attr()
-		{
-		}
-	}
 	
-	public class drawable
+	public partial class R
 	{
 		
-		// aapt resource value: 0x7f020000
-		public const int android = 2130837504;
-		
-		// aapt resource value: 0x7f020001
-		public const int background = 2130837505;
-		
-		// aapt resource value: 0x7f020002
-		public const int ic_launcher = 2130837506;
-		
-		// aapt resource value: 0x7f020003
-		public const int icon = 2130837507;
-		
-		// aapt resource value: 0x7f020004
-		public const int image_container = 2130837508;
-		
-		// aapt resource value: 0x7f020005
-		public const int sample_0 = 2130837509;
-		
-		// aapt resource value: 0x7f020006
-		public const int sample_1 = 2130837510;
-		
-		// aapt resource value: 0x7f020007
-		public const int sample_2 = 2130837511;
-		
-		// aapt resource value: 0x7f020008
-		public const int sample_3 = 2130837512;
-		
-		// aapt resource value: 0x7f020009
-		public const int sample_4 = 2130837513;
-		
-		// aapt resource value: 0x7f02000a
-		public const int sample_5 = 2130837514;
-		
-		// aapt resource value: 0x7f02000b
-		public const int sample_6 = 2130837515;
-		
-		// aapt resource value: 0x7f02000c
-		public const int sample_7 = 2130837516;
-		
-		private drawable()
+		public class attr
 		{
+			
+			private attr()
+			{
+			}
 		}
-	}
-	
-	public class layout
-	{
 		
-		// aapt resource value: 0x7f030000
-		public const int main = 2130903040;
-		
-		private layout()
+		public class drawable
 		{
+			
+			// aapt resource value: 0x7f020000
+			public const int android = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int background = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int ic_launcher = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int icon = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int image_container = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int sample_0 = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int sample_1 = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int sample_2 = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int sample_3 = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int sample_4 = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int sample_5 = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int sample_6 = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int sample_7 = 2130837516;
+			
+			private drawable()
+			{
+			}
 		}
-	}
-	
-	public class @string
-	{
 		
-		// aapt resource value: 0x7f040000
-		public const int app_name = 2130968576;
-		
-		// aapt resource value: 0x7f040001
-		public const int next_button = 2130968577;
-		
-		private @string()
+		public class layout
 		{
+			
+			// aapt resource value: 0x7f030000
+			public const int main = 2130903040;
+			
+			private layout()
+			{
+			}
 		}
-	}
-	
-	public class id
-	{
 		
-		// aapt resource value: 0x7f050000
-		public const int image_container = 2131034112;
-		
-		// aapt resource value: 0x7f050001
-		public const int image_view = 2131034113;
-		
-		// aapt resource value: 0x7f050002
-		public const int status_text = 2131034114;
-		
-		// aapt resource value: 0x7f050003
-		public const int next_button = 2131034115;
-		
-		private id()
+		public class @string
 		{
+			
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int next_button = 2130968577;
+			
+			private @string()
+			{
+			}
+		}
+		
+		public class id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int image_container = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int image_view = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int status_text = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int next_button = 2131034115;
+			
+			private id()
+			{
+			}
 		}
 	}
 }
