d352b3c949842d5b79ea723c181c66f8ea628a47
Ankit Jain <radical@corewars.org>
2010-08-18 02:25:20 +0530

    GLCube - Add animation, keeps rotating.

--- GLCube/PaintingView.cs
+++ GLCube/PaintingView.cs
@@ -12,6 +12,7 @@ namespace Mono.Samples.GLCube {
 
 	class PaintingView : AndroidGameView
 	{
+		float rotx, roty, rotz;
 		public PaintingView (IntPtr handle) : base (handle)
 		{ }
 
@@ -19,49 +20,22 @@ namespace Mono.Samples.GLCube {
 		{
 			base.OnLoad (e);
 
-			CreateFrameBuffer();
-			RenderCube ();
+			UpdateFrame += delegate {
+				rotx += 0.5f;
+				roty += 1.5f;
+				rotz += 0.9f;
+			};
+
+			RenderFrame += delegate {
+				RenderCube ();
+			};
+
+			// Run the render loop
+			Run ();
 		}
 
 		void RenderCube ()
 		{
-			float[] cube = {
-				-0.5f, 0.5f, 0.5f, // vertex[0]
-				0.5f, 0.5f, 0.5f, // vertex[1]
-				0.5f, -0.5f, 0.5f, // vertex[2]
-				-0.5f, -0.5f, 0.5f, // vertex[3]
-				-0.5f, 0.5f, -0.5f, // vertex[4]
-				0.5f, 0.5f, -0.5f, // vertex[5]
-				0.5f, -0.5f, -0.5f, // vertex[6]
-				-0.5f, -0.5f, -0.5f, // vertex[7]
-			};
-
-			byte[] triangles = {
-				1, 0, 2, // front
-				3, 2, 0,
-				6, 4, 5, // back
-				4, 6, 7,
-				4, 7, 0, // left
-				7, 3, 0,
-				1, 2, 5, //right
-				2, 6, 5,
-				0, 1, 5, // top
-				0, 5, 4,
-				2, 3, 6, // bottom
-				3, 7, 6,
-			};
-
-			float[] cubeColors = {
-				1.0f, 0.0f, 0.0f, 1.0f,
-				0.0f, 1.0f, 0.0f, 1.0f,
-				0.0f, 0.0f, 1.0f, 1.0f,
-				0.0f, 1.0f, 1.0f, 1.0f,
-				1.0f, 0.0f, 0.0f, 1.0f,
-				0.0f, 1.0f, 0.0f, 1.0f,
-				0.0f, 0.0f, 1.0f, 1.0f,
-				0.0f, 1.0f, 1.0f, 1.0f,
-			};
-
 			GL.Enable(All.CullFace);
 			GL.MatrixMode (All.Projection);
 			GL.LoadIdentity ();
@@ -69,8 +43,9 @@ namespace Mono.Samples.GLCube {
 
 			GL.MatrixMode (All.Modelview);
 			GL.LoadIdentity ();
-			GL.Rotate (35.0f, 1.0f, 0.0f, 0.0f);
-			GL.Rotate (15.0f, 0.0f, 1.0f, 0.0f);
+			GL.Rotate (rotx, 1.0f, 0.0f, 0.0f);
+			GL.Rotate (roty, 0.0f, 1.0f, 0.0f);
+			GL.Rotate (rotz, 0.0f, 1.0f, 0.0f);
 
 			GL.ClearColor (0, 0, 0, 1.0f);
 			GL.Clear ((uint) All.ColorBufferBit);
@@ -83,5 +58,42 @@ namespace Mono.Samples.GLCube {
 
 			SwapBuffers ();
 		}
+
+		float[] cube = {
+			-0.5f, 0.5f, 0.5f, // vertex[0]
+			0.5f, 0.5f, 0.5f, // vertex[1]
+			0.5f, -0.5f, 0.5f, // vertex[2]
+			-0.5f, -0.5f, 0.5f, // vertex[3]
+			-0.5f, 0.5f, -0.5f, // vertex[4]
+			0.5f, 0.5f, -0.5f, // vertex[5]
+			0.5f, -0.5f, -0.5f, // vertex[6]
+			-0.5f, -0.5f, -0.5f, // vertex[7]
+		};
+
+		byte[] triangles = {
+			1, 0, 2, // front
+			3, 2, 0,
+			6, 4, 5, // back
+			4, 6, 7,
+			4, 7, 0, // left
+			7, 3, 0,
+			1, 2, 5, //right
+			2, 6, 5,
+			0, 1, 5, // top
+			0, 5, 4,
+			2, 3, 6, // bottom
+			3, 7, 6,
+		};
+
+		float[] cubeColors = {
+			1.0f, 0.0f, 0.0f, 1.0f,
+			0.0f, 1.0f, 0.0f, 1.0f,
+			0.0f, 0.0f, 1.0f, 1.0f,
+			0.0f, 1.0f, 1.0f, 1.0f,
+			1.0f, 0.0f, 0.0f, 1.0f,
+			0.0f, 1.0f, 0.0f, 1.0f,
+			0.0f, 0.0f, 1.0f, 1.0f,
+			0.0f, 1.0f, 1.0f, 1.0f,
+		};
 	}
 }
