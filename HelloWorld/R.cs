681b0765e51b39a945454934c285c256dd487f82
Jonathan Pryor <jonpryor@vt.edu>
2010-08-17 14:51:05 -0400

    Set <RootNamespace/> to an actually used namespace.
    Recent versions of Novell.MonoDroid.Build.Tasks.dll place the generated R type
    into the <RootNamespace/> namespace, so if this namespace isn't actually used
    by the program we get compiler errors.

--- HelloWorld/R.cs
+++ HelloWorld/R.cs
@@ -1,47 +1,52 @@
-public partial class R
+namespace Mono.Samples.HelloWorld
 {
 	
-	public class attr
-	{
-		
-		private attr()
-		{
-		}
-	}
 	
-	public class drawable
+	public partial class R
 	{
 		
-		// aapt resource value: 0x7f020000
-		public const int icon = 2130837504;
-		
-		private drawable()
+		public class attr
 		{
+			
+			private attr()
+			{
+			}
 		}
-	}
-	
-	public class layout
-	{
 		
-		// aapt resource value: 0x7f030000
-		public const int main = 2130903040;
-		
-		private layout()
+		public class drawable
 		{
+			
+			// aapt resource value: 0x7f020000
+			public const int icon = 2130837504;
+			
+			private drawable()
+			{
+			}
 		}
-	}
-	
-	public class @string
-	{
 		
-		// aapt resource value: 0x7f040000
-		public const int hello = 2130968576;
-		
-		// aapt resource value: 0x7f040001
-		public const int app_name = 2130968577;
-		
-		private @string()
+		public class layout
 		{
+			
+			// aapt resource value: 0x7f030000
+			public const int main = 2130903040;
+			
+			private layout()
+			{
+			}
+		}
+		
+		public class @string
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int hello = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int app_name = 2130968577;
+			
+			private @string()
+			{
+			}
 		}
 	}
 }
