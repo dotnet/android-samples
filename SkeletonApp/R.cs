315c293f54cc8419ea4a89dd3c5ade79387c0f6a
Jonathan Pryor <jonpryor@vt.edu>
2010-08-17 15:24:54 -0400

    Set <RootNamespace/> to an actually used namespace.
    Recent versions of Novell.MonoDroid.Build.Tasks.dll place the generated
    R type into the <RootNamespace/> namespace, so if this namespace isn't
    actually used by the program we get compiler errors.

--- SkeletonApp/R.cs
+++ SkeletonApp/R.cs
@@ -1,104 +1,109 @@
-public partial class R
+namespace Mono.Samples.SkeletonApp
 {
 	
-	public class attr
-	{
-		
-		private attr()
-		{
-		}
-	}
 	
-	public class drawable
+	public partial class R
 	{
 		
-		// aapt resource value: 0x7f020000
-		public const int violet = 2130837504;
-		
-		// aapt resource value: 0x7f020001
-		public const int semi_black = 2130837505;
-		
-		private drawable()
+		public class attr
 		{
+			
+			private attr()
+			{
+			}
 		}
-	}
-	
-	public class layout
-	{
 		
-		// aapt resource value: 0x7f030000
-		public const int skeleton_activity = 2130903040;
-		
-		private layout()
+		public class drawable
 		{
+			
+			// aapt resource value: 0x7f020000
+			public const int violet = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int semi_black = 2130837505;
+			
+			private drawable()
+			{
+			}
 		}
-	}
-	
-	public class color
-	{
 		
-		// aapt resource value: 0x7f040000
-		public const int red = 2130968576;
-		
-		private color()
+		public class layout
 		{
+			
+			// aapt resource value: 0x7f030000
+			public const int skeleton_activity = 2130903040;
+			
+			private layout()
+			{
+			}
 		}
-	}
-	
-	public class @string
-	{
 		
-		// aapt resource value: 0x7f050000
-		public const int skeleton_app = 2131034112;
-		
-		// aapt resource value: 0x7f050001
-		public const int back = 2131034113;
-		
-		// aapt resource value: 0x7f050002
-		public const int clear = 2131034114;
-		
-		// aapt resource value: 0x7f050003
-		public const int main_label = 2131034115;
-		
-		private @string()
+		public class color
 		{
+			
+			// aapt resource value: 0x7f040000
+			public const int red = 2130968576;
+			
+			private color()
+			{
+			}
 		}
-	}
-	
-	public class style
-	{
 		
-		// aapt resource value: 0x7f060000
-		public const int ActionButton = 2131099648;
-		
-		// aapt resource value: 0x7f060001
-		public const int TextAppearance = 2131099649;
-		
-		// aapt resource value: 0x7f060002
-		public const int TextAppearance_ActionButton = 2131099650;
-		
-		private style()
+		public class @string
 		{
+			
+			// aapt resource value: 0x7f050000
+			public const int skeleton_app = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int back = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int clear = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int main_label = 2131034115;
+			
+			private @string()
+			{
+			}
 		}
-	}
-	
-	public class id
-	{
 		
-		// aapt resource value: 0x7f070000
-		public const int editor = 2131165184;
-		
-		// aapt resource value: 0x7f070001
-		public const int back = 2131165185;
-		
-		// aapt resource value: 0x7f070002
-		public const int image = 2131165186;
-		
-		// aapt resource value: 0x7f070003
-		public const int clear = 2131165187;
-		
-		private id()
+		public class style
 		{
+			
+			// aapt resource value: 0x7f060000
+			public const int ActionButton = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int TextAppearance = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int TextAppearance_ActionButton = 2131099650;
+			
+			private style()
+			{
+			}
+		}
+		
+		public class id
+		{
+			
+			// aapt resource value: 0x7f070000
+			public const int editor = 2131165184;
+			
+			// aapt resource value: 0x7f070001
+			public const int back = 2131165185;
+			
+			// aapt resource value: 0x7f070002
+			public const int image = 2131165186;
+			
+			// aapt resource value: 0x7f070003
+			public const int clear = 2131165187;
+			
+			private id()
+			{
+			}
 		}
 	}
 }
