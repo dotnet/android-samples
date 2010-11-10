65c12c71c991b735300ff7b72ba3846b1f828060
Jonathan Pryor <jonpryor@vt.edu>
2010-11-10 10:50:19 -0500

    Update HelloWorld for Preview 7.

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -8,10 +8,6 @@ namespace Mono.Samples.HelloWorld
 	[Activity (Label = "Hello World Demo", MainLauncher = true)]
 	public class HelloAndroid : Activity
 	{
-		public HelloAndroid (IntPtr handle) : base (handle)
-		{
-		}
-
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
