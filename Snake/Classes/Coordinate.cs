970cd2e843ec88e43bfb5a42a4e9522c9938bde1
Jonathan Pobst <monkey@jpobst.com>
2010-12-07 09:48:44 -0600

    Commit Snake sample.

---
+++ Snake/Classes/Coordinate.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Mono.Samples.Snake
+{
+	// Simple class containing two integer values and a comparison function.
+	public class Coordinate
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public Coordinate (int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public override bool Equals (object obj)
+		{
+			Coordinate other = (Coordinate)obj;
+
+			return X == other.X && Y == other.Y;
+		}
+
+		public override String ToString ()
+		{
+			return string.Format ("Coordinate: [{0}, {1}]", X, Y);
+		}
+
+		// Given a List of coordinates, we need to flatten them into an array of
+		// ints before we can stuff them into a map for flattening and storage.
+		public static int[] ListToArray (List<Coordinate> list)
+		{
+			List<int> array = new List<int> ();
+
+			foreach (Coordinate c in list) {
+				array.Add (c.X);
+				array.Add (c.Y);
+			}
+
+			return array.ToArray ();
+		}
+
+		// Given a flattened array of ordinate pairs, we reconstitute
+		//  them into a List of Coordinate objects
+		public static List<Coordinate> ArrayToList (int[] array)
+		{
+			List<Coordinate> list = new List<Coordinate> ();
+
+			for (int index = 0; index < array.Length; index += 2)
+				list.Add (new Coordinate (array[index], array[index + 1]));
+
+			return list;
+		}
+	}
+}
---
+++ Snake/Classes/TileView.cs
@@ -0,0 +1,125 @@
+﻿/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Util;
+using Android.Views;
+
+namespace Mono.Samples.Snake
+{
+	// TileView: a View-variant designed for handling arrays of "icons" or other
+	// drawables.
+	public class TileView : View
+	{
+		protected static int tile_size = 12;
+
+		protected static int x_tile_count;
+		protected static int y_tile_count;
+
+		private static int x_offset; 
+		private static int y_offset;
+
+		private Bitmap[] tile_bitmaps;
+		private TileType[,] tiles;
+
+		private Paint paint = new Paint ();
+
+		public TileView (Context context, IAttributeSet attrs, int defStyle) :
+			base (context, attrs, defStyle)
+		{
+			//TypedArray a = context.ObtainStyledAttributes (attrs, R.styleable.TileView);
+			//a.GetInt (R.styleable.TileView_tileSize, 12);
+			//a.Recycle ();
+		}
+
+		public TileView (Context context, IAttributeSet attrs) :
+			base (context, attrs)
+		{
+			//TypedArray a = context.ObtainStyledAttributes (attrs, R.styleable.TileView);
+			//a.GetInt (R.styleable.TileView_tileSize, 12);
+			//a.Recycle ();
+		}
+
+		#region Public Methods
+		// Resets the internal array of Bitmaps used for drawing tiles, and
+		// sets the maximum index of tiles to be inserted
+		public void ResetTiles (int tileCount)
+		{
+			tile_bitmaps = new Bitmap[tileCount];
+		}
+
+		// Function to set the specified Drawable as the tile for a particular
+		// integer key.
+		public void LoadTile (TileType type, Drawable tile)
+		{
+			Bitmap bitmap = Bitmap.CreateBitmap (tile_size, tile_size, Bitmap.Config.Argb8888);
+			Canvas canvas = new Canvas (bitmap);
+
+			tile.SetBounds (0, 0, tile_size, tile_size);
+			tile.Draw (canvas);
+
+			tile_bitmaps[(int)type] = bitmap;
+		}
+
+		// Resets all tiles to 0 (empty)
+		public void ClearTiles ()
+		{
+			for (int x = 0; x < x_tile_count; x++)
+				for (int y = 0; y < y_tile_count; y++)
+					SetTile (0, x, y);
+		}
+
+		// Used to indicate that a particular tile (set with loadTile and referenced
+		// by an integer) should be drawn at the given x/y coordinates during the
+		// next invalidate/draw cycle.
+		public void SetTile (TileType tile, int x, int y)
+		{
+			tiles[x, y] = tile;
+		}
+		#endregion
+
+		#region Protected Methods
+		protected override void OnSizeChanged (int w, int h, int oldw, int oldh)
+		{
+			x_tile_count = (int)System.Math.Floor ((double)w / tile_size);
+			y_tile_count = (int)System.Math.Floor ((double)h / tile_size);
+
+			x_offset = ((w - (tile_size * x_tile_count)) / 2);
+			y_offset = ((h - (tile_size * y_tile_count)) / 2);
+
+			tiles = new TileType[x_tile_count, y_tile_count];
+
+			ClearTiles ();
+		}
+
+		protected override void OnDraw (Canvas canvas)
+		{
+			base.OnDraw (canvas);
+
+			for (int x = 0; x < x_tile_count; x += 1)
+				for (int y = 0; y < y_tile_count; y += 1)
+					if (tiles[x, y] > 0)
+						canvas.DrawBitmap (tile_bitmaps[(int)tiles[x, y]],
+							    x_offset + x * tile_size,
+							    y_offset + y * tile_size,
+							    paint);
+		}
+		#endregion
+	}
+}
