674b2c2569d1b9bc51d5c5e64c07419298eae91a
Jonathan Pobst <monkey@jpobst.com>
2012-01-17 14:11:27 -0600

    Use Hugo's spiffy default Xamarin icons.

--- AccelerometerPlay/Properties/AssemblyInfo.cs
+++ AccelerometerPlay/Properties/AssemblyInfo.cs
@@ -35,3 +35,4 @@ using Android.App;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion ("1.0.0.0")]
 [assembly: AssemblyFileVersion ("1.0.0.0")]
+[assembly: Application (Icon = "@drawable/icon")]
