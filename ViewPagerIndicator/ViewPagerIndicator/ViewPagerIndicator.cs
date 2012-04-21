c0fd2204cd4cb1ff3db6af110097172f47dfa7f3
Jonathan Pryor <jonpryor@vt.edu>
2012-04-20 22:23:27 -0400

    [ViewPagerIndicator] Fix Listener event related changes.

--- ViewPagerIndicator/ViewPagerIndicator/ViewPagerIndicator.cs
+++ ViewPagerIndicator/ViewPagerIndicator/ViewPagerIndicator.cs
@@ -37,7 +37,7 @@ namespace ViewPagerIndicator
 			ListAdapter = new ArrayAdapter<ActivityListItem> (this, Android.Resource.Layout.SimpleListItem1, Android.Resource.Id.Text1, items);
 
 			// Launch the new activity when the list is clicked
-			ListView.ItemClick += delegate (object sender, ItemEventArgs args) {
+			ListView.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args) {
 				var item = (ActivityListItem)(sender as ListView).GetItemAtPosition (args.Position);
 				LaunchActivityItem (item);
 			};
