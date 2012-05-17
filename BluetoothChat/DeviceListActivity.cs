d06901467141f6bdbb9c77bc141f41d4474cf5f6
Tom Opgenorth <tom@opgenorth.net>
2012-05-16 19:22:59 -0600

    Fixing up the compile errors in BluetoothChat

--- BluetoothChat/DeviceListActivity.cs
+++ BluetoothChat/DeviceListActivity.cs
@@ -152,7 +152,7 @@ namespace BluetoothChat
 		/// <summary>
 		/// The on-click listener for all devices in the ListViews
 		/// </summary>
-		void DeviceListClick (object sender, ItemEventArgs e)
+        void DeviceListClick(object sender, AdapterView.ItemClickEventArgs e)
 		{
 			// Cancel discovery because it's costly and we're about to connect
 			btAdapter.CancelDiscovery ();
