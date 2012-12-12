8718c1e31c8052c51b0d5f717cb74b59b1f1a9fa
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2012-12-13 04:48:20 +0900

    Add "AndroidResource in managed library" (libraries can embed resources) sample.

---
+++ ManagedAndroidResourceTest/ManagedAndroidResourceApp/Activity1.cs
@@ -0,0 +1,36 @@
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace ManagedAndroidResourceApp
+{
+	[Activity (Label = "ManagedAndroidResourceApp", MainLauncher = true)]
+	public class Activity1 : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			button.Text = this.GetString (Resource.String.library_name);
+			
+			button.Click += delegate {
+				button.Text = string.Format ("{0} clicks!", count++);
+			};
+		}
+	}
+}
+
+
