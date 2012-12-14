d4565ad9e593acc061a17850592ec4ad56c1c400
Tom Opgenorth <tom@opgenorth.net>
2012-12-14 10:35:07 -0700

    Changing the MapActivity to use Google Maps v2
    Just displaying the map for now.

--- MapsAndLocationDemo_v2/MapsAndLocationDemo/SampleMapActivity.cs
+++ MapsAndLocationDemo_v2/MapsAndLocationDemo/SampleMapActivity.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 
 using Android.App;
+using Android.Gms.Maps;
+using Android.Support.V4.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
@@ -14,54 +16,54 @@ using Android.GoogleMaps;
 namespace MapsAndLocationDemo
 {
     [Activity (Label = "SampleMapActivity")]         
-    public class SampleMapActivity : MapActivity
+    public class SampleMapActivity : FragmentActivity 
     {
         protected override void OnCreate (Bundle bundle)
         {
             base.OnCreate (bundle);
-
             SetContentView (Resource.Layout.MapLayout);
-         
-            var map = FindViewById<MapView> (Resource.Id.map);
-         
-            map.Clickable = true;
-            map.Traffic = false;
-            map.Satellite = true;
-         
-            map.SetBuiltInZoomControls (true);   
-            map.Controller.SetZoom (10);
-            map.Controller.SetCenter (new GeoPoint ((int)42.374260E6, (int)-71.120824E6));
-         
-            var zoomInButton = FindViewById<Button> (Resource.Id.zoomInButton);
-            var zoomOutButton = FindViewById<Button> (Resource.Id.zoomOutButton);
-            var animateButton = FindViewById<Button> (Resource.Id.animateButton);
-         
-            zoomInButton.Click += (sender, e) => {
-                map.Controller.ZoomIn ();
-                //map.Controller.ZoomInFixing (200, 200);
-            };
-         
-            zoomOutButton.Click += (sender, e) => {
-                map.Controller.ZoomOut ();
-                //map.Controller.ZoomOutFixing (200, 200);
-            };
-         
-            animateButton.Click += (sender, e) => {
-                //map.Controller.AnimateTo(new GeoPoint ((int)40.741773E6, (int)-74.004986E6));
-             
-                map.Controller.AnimateTo (
-                 new GeoPoint ((int)40.741773E6, (int)-74.004986E6), () => {
-                    var toast = Toast.MakeText (this, "Welcome to NY", ToastLength.Short);
-                    toast.Show ();
-                });
-                                      
-            };
-        }
 
-        protected override bool IsRouteDisplayed {
-            get {
-                return false;
-            }
+            var fragTx = SupportFragmentManager.BeginTransaction ();
+            var mapFragment = SupportMapFragment.NewInstance ();
+            fragTx.Add (Resource.Id.map, mapFragment, "map");
+            fragTx.Commit();
+
+
+
+//            var map = FindViewById<MapView> (Resource.Id.map);
+//         
+//            map.Clickable = true;
+//            map.Traffic = false;
+//            map.Satellite = true;
+//         
+//            map.SetBuiltInZoomControls (true);   
+//            map.Controller.SetZoom (10);
+//            map.Controller.SetCenter (new GeoPoint ((int)42.374260E6, (int)-71.120824E6));
+//         
+//            var zoomInButton = FindViewById<Button> (Resource.Id.zoomInButton);
+//            var zoomOutButton = FindViewById<Button> (Resource.Id.zoomOutButton);
+//            var animateButton = FindViewById<Button> (Resource.Id.animateButton);
+//         
+//            zoomInButton.Click += (sender, e) => {
+//                map.Controller.ZoomIn ();
+//                //map.Controller.ZoomInFixing (200, 200);
+//            };
+//         
+//            zoomOutButton.Click += (sender, e) => {
+//                map.Controller.ZoomOut ();
+//                //map.Controller.ZoomOutFixing (200, 200);
+//            };
+//         
+//            animateButton.Click += (sender, e) => {
+//                //map.Controller.AnimateTo(new GeoPoint ((int)40.741773E6, (int)-74.004986E6));
+//             
+//                map.Controller.AnimateTo (
+//                 new GeoPoint ((int)40.741773E6, (int)-74.004986E6), () => {
+//                    var toast = Toast.MakeText (this, "Welcome to NY", ToastLength.Short);
+//                    toast.Show ();
+//                });
+//                                      
+//            };
         }
     }
 }
