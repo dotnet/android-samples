dotnet/android-samples

commit d00153b1433e7592d08b16f94d23fbb80605de2a

Resource folder cleanup

GestureBuilder/GestureBuilder/GestureBuilderActivity.cs
@@ -45,7 +45,7 @@ using Environment = Android.OS.Environment;
 namespace GestureBuilder
 {
 	[Activity (Label = "@string/application_name",
-               Icon="@drawable/ic_gesturebuilder")]
+               Icon="@drawable/icon"]
 	[IntentFilter (new string [] {"android.intent.action.MAIN"}, Categories = new string [] {
 		Intent.CategoryLauncher, Intent.CategoryDefault})]
 	public class GestureBuilderActivity : ListActivity
@@ -456,4 +456,5 @@ namespace GestureBuilder
 			}
 		}
 	}
-}
+
+	}

commit e347c85ac252c6166d90ecb2e6c2cd8dc82554b3

[GestureBuilder] Fix build break.
Fixes: https://bugzilla.xamarin.com/show_bug.cgi?id=9822

Code was broken in commit d00153b1.

GestureBuilder/GestureBuilder/GestureBuilderActivity.cs
@@ -45,7 +45,7 @@ using Environment = Android.OS.Environment;
 namespace GestureBuilder
 {
 	[Activity (Label = "@string/application_name",
-               Icon="@drawable/icon"]
+               Icon="@drawable/icon")]
 	[IntentFilter (new string [] {"android.intent.action.MAIN"}, Categories = new string [] {
 		Intent.CategoryLauncher, Intent.CategoryDefault})]
 	public class GestureBuilderActivity : ListActivity
@@ -456,5 +456,4 @@ namespace GestureBuilder
 			}
 		}
 	}
-
-	}
+}