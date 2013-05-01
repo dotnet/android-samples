661324465f878430c2c4e494797a34e9df53f033
Tom Opgenorth <tom@opgenorth.net>
2013-05-01 11:20:44 -0600

    Resize the picture that was taken by the camera before displaying it.

---
+++ CameraAppDemo/BitmapHelpers.cs
@@ -0,0 +1,36 @@
+namespace CameraAppDemo
+{
+    using System.IO;
+
+    using Android.Graphics;
+
+    public static class BitmapHelpers
+    {
+        public static Bitmap LoadAndResizeBitmap(this string fileName, int width, int height)
+        {
+            // First we get the the dimensions of the file on disk
+            BitmapFactory.Options options = new BitmapFactory.Options { InJustDecodeBounds = true };
+            BitmapFactory.DecodeFile(fileName, options);
+
+            // Next we calculate the ratio that we need to resize the image by
+            // in order to fit the requested dimensions.
+            int outHeight = options.OutHeight;
+            int outWidth = options.OutWidth;
+            int inSampleSize = 1;
+
+            if (outHeight > height || outWidth > width)
+            {
+                inSampleSize = outWidth > outHeight
+                                   ? outHeight / height
+                                   : outWidth / width;
+            }
+
+            // Now we will load the image and have BitmapFactory resize it for us.
+            options.InSampleSize = inSampleSize;
+            options.InJustDecodeBounds = false;
+            Bitmap resizedBitmap = BitmapFactory.DecodeFile(fileName, options);
+
+            return resizedBitmap;
+        }
+    }
+}
---
+++ CameraAppDemo/MainActivity.cs
@@ -0,0 +1,90 @@
+namespace CameraAppDemo
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Android.App;
+    using Android.Content;
+    using Android.Content.PM;
+    using Android.Graphics;
+    using Android.OS;
+    using Android.Provider;
+    using Android.Widget;
+
+    using Java.IO;
+
+    using Environment = Android.OS.Environment;
+    using Uri = Android.Net.Uri;
+
+    [Activity(Label = "Camera App Demo", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private File _dir;
+        private File _file;
+        private ImageView _imageView;
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            // make it available in the gallery
+            Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            Uri contentUri = Uri.FromFile(_file);
+            mediaScanIntent.SetData(contentUri);
+            SendBroadcast(mediaScanIntent);
+
+            // display in ImageView. We will resize the bitmap to fit the display
+            // Loading the full sized image will consume to much memory 
+            // and cause the application to crash.
+            int height = Resources.DisplayMetrics.HeightPixels;
+            int width = _imageView.Height ;
+            using (Bitmap bitmap = _file.Path.LoadAndResizeBitmap(width, height))
+            {
+                _imageView.SetImageBitmap(bitmap);
+            }
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            if (IsThereAnAppToTakePictures())
+            {
+                CreateDirectoryForPictures();
+
+                Button button = FindViewById<Button>(Resource.Id.myButton);
+                _imageView = FindViewById<ImageView>(Resource.Id.imageView1);
+
+                button.Click += TakeAPicture;
+            }
+        }
+
+        private void CreateDirectoryForPictures()
+        {
+            _dir = new File(Environment.GetExternalStoragePublicDirectory(Environment.DirectoryPictures), "CameraAppDemo");
+            if (_dir.Exists())
+            {
+                _dir.Mkdirs();
+            }
+        }
+
+        private bool IsThereAnAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities = PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+
+        private void TakeAPicture(object sender, EventArgs eventArgs)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+
+            _file = new File(_dir, String.Format("myPhoto_{0}.jpg", Guid.NewGuid()));
+
+            intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(_file));
+
+            StartActivityForResult(intent, 0);
+        }
+    }
+}
