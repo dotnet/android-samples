15eddd343ba331bed1c8d5b36b5f278b1d3acbec
Tom Opgenorth <tom@opgenorth.net>
2013-05-02 13:11:41 -0600

    Breaking up the Maps Demo into it's own project.

---
+++ MapsAndLocationDemo_v2/SimpleMapDemo/MainActivity.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleMapDemo
+{
+    using Android.App;
+    using Android.OS;
+    using Android.Widget;
+
+    [Activity(Label = "SimpleMapDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : ListActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+        }
+    }
+}
