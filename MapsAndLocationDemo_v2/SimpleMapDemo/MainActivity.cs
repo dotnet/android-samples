8d24f33dd80cd334203566e13c7f5857ff42b944
Tom Opgenorth <tom@opgenorth.net>
2013-05-27 15:38:23 -0600

    Adding in a check for the GooglePlay Services.

--- MapsAndLocationDemo_v2/SimpleMapDemo/MainActivity.cs
+++ MapsAndLocationDemo_v2/SimpleMapDemo/MainActivity.cs
@@ -6,7 +6,9 @@
 
     using Android.App;
     using Android.Content;
+	using Android.Gms.Common;
     using Android.OS;
+	using Android.Util;
     using Android.Views;
     using Android.Widget;
 
@@ -15,21 +17,74 @@
     [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : ListActivity
     {
+		public static readonly int InstallGooglePlayServicesId = 1000;
+
         private List<SampleActivity> _activities;
+		private bool _isGooglePlayServicesInstalled;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            _activities = new List<SampleActivity>
-                              {
-                                  new SampleActivity(Resource.String.mapsAppText, Resource.String.mapsAppTextDescription, null),
-								  new SampleActivity(Resource.String.basic_map, Resource.String.basic_map_description, typeof(BasicDemoActivity)),
-                                  new SampleActivity(Resource.String.activity_label_samplemap, Resource.String.showMapActivityDescription, typeof(MapWithMarkersActivity)),
-                                  new SampleActivity(Resource.String.activity_label_mapwithmarkers, Resource.String.showMapWithOverlaysDescription, typeof(SampleMapActivity))
-                              };
-
-            ListAdapter = new SimpleMapDemoActivityAdapter(this, _activities);
+			_isGooglePlayServicesInstalled = TestIfGooglePlayServicesIsInstalled ();
+			InitializeListView ();
         }
+
+		private void InitializeListView()
+		{
+			if (_isGooglePlayServicesInstalled) 
+			{
+				_activities = new List<SampleActivity>
+				{
+					new SampleActivity(Resource.String.mapsAppText, Resource.String.mapsAppTextDescription, null),
+					new SampleActivity(Resource.String.basic_map, Resource.String.basic_map_description, typeof(BasicDemoActivity)),
+					new SampleActivity(Resource.String.activity_label_samplemap, Resource.String.showMapActivityDescription, typeof(MapWithMarkersActivity)),
+					new SampleActivity(Resource.String.activity_label_mapwithmarkers, Resource.String.showMapWithOverlaysDescription, typeof(SampleMapActivity))
+				};
+
+				ListAdapter = new SimpleMapDemoActivityAdapter(this, _activities);
+			}
+			else
+			{
+				Log.Error ("MainActivity", "Google Play Services is not installed");
+				ListAdapter = new SimpleMapDemoActivityAdapter (this, null);
+			}
+		}
+
+		protected override void OnActivityResult (int requestCode, Result resultCode, Intent data)
+		{
+			switch (resultCode) 
+			{
+			case Result.Ok:
+				// Try again.
+				_isGooglePlayServicesInstalled = true;
+				break;
+
+			default:
+				Log.Debug ("MainActivity", "Unknown resultCode {0} for request {1}", resultCode, requestCode);
+				break;
+			}
+		}
+
+		private bool TestIfGooglePlayServicesIsInstalled()
+		{
+			int queryResult = GooglePlayServicesUtil.IsGooglePlayServicesAvailable (this);
+			if (queryResult == ConnectionResult.Success)
+			{
+				Log.Info ("SimpleMapDemo", "Google Play Services is installed on this device.");
+				return true;
+			}
+
+			if (GooglePlayServicesUtil.IsUserRecoverableError (queryResult)) 
+			{
+				string errorString = GooglePlayServicesUtil.GetErrorString (queryResult);
+				Log.Error ("SimpleMapDemo", "There is a problem with Google Play Services on this device: {0} - {1}", queryResult, errorString);
+				Dialog errorDialog = GooglePlayServicesUtil.GetErrorDialog(queryResult, this, InstallGooglePlayServicesId);
+				ErrorDialogFragment dialogFrag = new ErrorDialogFragment(errorDialog);
+
+				dialogFrag.Show (FragmentManager, "GooglePlayServicesDialog");
+			}
+			return false;
+		}
 
         protected override void OnListItemClick(ListView l, View v, int position, long id)
         {
@@ -46,6 +101,24 @@
         }
     }
 
+	internal class ErrorDialogFragment :DialogFragment
+	{
+		public ErrorDialogFragment(Dialog dialog) 
+		{
+			Dialog = dialog;
+		}
+
+		public Dialog Dialog { get; private set; }
+
+		public override Dialog OnCreateDialog (Bundle savedInstanceState)
+		{
+			return Dialog;
+
+		}
+
+
+	}
+
     internal class SimpleMapDemoActivityAdapter : BaseAdapter<SampleActivity>
     {
         private readonly List<SampleActivity> _activities;
@@ -54,7 +127,14 @@
         public SimpleMapDemoActivityAdapter(Context context, IEnumerable<SampleActivity> sampleActivities)
         {
             _context = context;
-            _activities = sampleActivities.ToList();
+			if (sampleActivities == null)
+			{
+				_activities = new List<SampleActivity>(0);
+			}
+			else 
+			{
+            	_activities = sampleActivities.ToList();
+			}
         }
 
         public override int Count { get { return _activities.Count; } }
