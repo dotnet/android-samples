220fad3559c32fa1556cd582fdc18edddfc0cfbc
olegoid <olegoid91@gmail.com>
2013-12-10 20:10:43 +0400

    [GLNativeES30] OpenTK 3.0 version of GLNativeES

---
+++ GLNativeES30/MyGLSurfaceView.cs
@@ -0,0 +1,28 @@
+using System;
+using Android.Views;
+using Android.Content;
+using Android.Util;
+using Android.Opengl;
+
+namespace GLNativeES30
+{
+	class MyGLSurfaceView : GLSurfaceView
+	{
+		private MyGLRenderer mRenderer;
+		private const float TOUCH_SCALE_FACTOR = 180.0f / 320;
+
+		public MyGLSurfaceView (Context context) : base (context)
+		{
+			// Create an OpenGL ES 3.0 context.
+			SetEGLContextClientVersion (3);
+
+			// Set the Renderer for drawing on the GLSurfaceView
+			mRenderer = new MyGLRenderer ();
+			SetRenderer (mRenderer);
+
+			// Render the view only when there is a change in the drawing data
+			this.RenderMode = Rendermode.WhenDirty;
+		}
+	}
+}
+
