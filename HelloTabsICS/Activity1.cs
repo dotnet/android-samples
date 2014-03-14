cbe3e6c3a61a05f3bfca78f473c8b26922225e31
TecSet <cm-develop@live.de>
2014-03-14 16:20:15 +0100

    Fix: Orientation Change brakes tabs
    If an Orientation Change is performed, the Tab changes back to the first one even if a differnt one was selected. Further more, the old Tab which was displayed before the change will be displayed behind the new one so that there are two Tabs which are displayed at the same time.

--- HelloTabsICS/Activity1.cs
+++ HelloTabsICS/Activity1.cs
@@ -22,7 +22,18 @@ namespace HelloTabsICS
             this.ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
                  
             AddTab ("Tab 1", Resource.Drawable.ic_tab_white, new SampleTabFragment ());
-            AddTab ("Tab 2", Resource.Drawable.ic_tab_white, new SampleTabFragment ());
+            AddTab ("Tab 2", Resource.Drawable.ic_tab_white, new SampleTabFragment2 ());
+
+            if (bundle != null)
+                this.ActionBar.SelectTab(this.ActionBar.GetTabAt(bundle.GetInt("tab")));
+
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            outState.PutInt("tab", this.ActionBar.SelectedNavigationIndex);
+
+            base.OnSaveInstanceState(outState);
         }
         
         void AddTab (string tabText, int iconResourceId, Fragment view)
@@ -32,7 +43,11 @@ namespace HelloTabsICS
             tab.SetIcon (Resource.Drawable.ic_tab_white);
             
             // must set event handler before adding tab
-            tab.TabSelected += delegate(object sender, ActionBar.TabEventArgs e) {              
+            tab.TabSelected += delegate(object sender, ActionBar.TabEventArgs e)
+            {
+                var fragment = this.FragmentManager.FindFragmentById(Resource.Id.fragmentContainer);
+                if (fragment != null)
+                    e.FragmentTransaction.Remove(fragment);         
                 e.FragmentTransaction.Add (Resource.Id.fragmentContainer, view);
             };
             tab.TabUnselected += delegate(object sender, ActionBar.TabEventArgs e) {
@@ -51,6 +66,20 @@ namespace HelloTabsICS
                 var view = inflater.Inflate (Resource.Layout.Tab, container, false);
                 var sampleTextView = view.FindViewById<TextView> (Resource.Id.sampleTextView);             
                 sampleTextView.Text = "sample fragment text";
+
+                return view;
+            }
+        }
+
+        class SampleTabFragment2 : Fragment
+        {
+            public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+            {
+                base.OnCreateView(inflater, container, savedInstanceState);
+
+                var view = inflater.Inflate(Resource.Layout.Tab, container, false);
+                var sampleTextView = view.FindViewById<TextView>(Resource.Id.sampleTextView);
+                sampleTextView.Text = "sample fragment text 2";
 
                 return view;
             }
