410001ff1b6e1801c7fdfafd933fef137c5dee2a
Fabio Nuno <nuno@ringteam.com.br>
2014-05-11 11:05:33 -0300

    Updated SlidingMenu library and Fixed Sample
    Updated SlidingMenu library with the most recent version of jfeinstein10
    
    Improved library binds to support Open and Close listeners.
    
    Fixed Sliding Menu sample and added some few options to demonstrate the
    library

--- SlidingMenu/SlidingMenuExample/MainActivity.cs
+++ SlidingMenu/SlidingMenuExample/MainActivity.cs
@@ -7,15 +7,14 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-using Com.Slidingmenu.Lib.App;
+using Com.SlidingMenu.Lib.App;
+using Com.SlidingMenu.Lib;
 
 namespace SlidingMenuExample
 {
 	[Activity (Label = "SlidingMenuExample", MainLauncher = true)]
-	public class Activity1 : SlidingActivity
+	public class Activity1 : SlidingActivity, SlidingMenu.IOnClosedListener, SlidingMenu.IOnCloseListener, SlidingMenu.IOnOpenListener, SlidingMenu.IOnOpenedListener
 	{
-		int count = 1;
-
 		public override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
@@ -24,16 +23,68 @@ namespace SlidingMenuExample
 			SetContentView(Resource.Layout.Main);
 			SetBehindContentView(Resource.Layout.menu);
 
+			this.SlidingMenu.SetFadeEnabled (true);
+			this.SlidingMenu.SetFadeDegree(0.35f);
+
+			this.SlidingMenu.SetOnCloseListener (this);
+			this.SlidingMenu.SetOnOpenedListener (this);
+			this.SlidingMenu.SetOnOpenListener (this);
+			this.SlidingMenu.SetOnOpenedListener (this);
 
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button>(Resource.Id.myButton);
-			
-			button.Click += (object sender, EventArgs e) => 
-			{
-				button.Text = string.Format("{0} clicks!", count++);
+
+			Button back = FindViewById<Button> (Resource.Id.Button5);
+			CheckBox marginCheck = FindViewById<CheckBox> (Resource.Id.marginCheck);
+			RadioGroup slideFromRadios = FindViewById<RadioGroup>(Resource.Id.slideFromOptions);
+
+			slideFromRadios.CheckedChange += (object sender, RadioGroup.CheckedChangeEventArgs e) => {
+				if (e.CheckedId == Resource.Id.modeLeftRadio)
+					this.SlidingMenu.Mode = 0; //LEFT
+				else
+					this.SlidingMenu.Mode = 1; //RIGHT
 			};
+
+			marginCheck.Click += (object sender, EventArgs e) => {
+
+				this.SlidingMenu.BehindOffset= (int)((marginCheck.Checked) ? Resources.GetDimension(Resource.Dimension.slidingMargin) : 0) ;
+			};
+
+			back.Click += MenuToggle;
+			button.Click += MenuToggle;
+
+
+
 		}
+
+		void MenuToggle(object sender, EventArgs e)
+		{
+			this.Toggle ();
+		}
+	
+	
+		#region Close and Open listener
+		public void OnOpen ()
+		{
+			Console.Write("Open");
+		}
+
+		public void OnOpened ()
+		{
+			Console.Write("Opened");
+		}
+
+		public void OnClose ()
+		{
+			Console.Write("Close");
+		}
+
+		public void OnClosed ()
+		{
+			Console.Write("Closed");
+		}
+		#endregion
 	}
 }
 
