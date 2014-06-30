0f702ccc4639b8aad8e760ad1f8c9f2c1671d861
ChrisBlackman <Chris@Christophers-iMac.local>
2014-06-30 17:47:10 -0400

    Another file?
    Not sure why the GitHub client is making me do so many commits.

--- JniDemo/Activity1.cs
+++ JniDemo/Activity1.cs
@@ -7,7 +7,7 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-namespace GoogleMaps
+namespace JNIDemo
 {
 	[Activity(Label = "JNI Test", MainLauncher = true)]
 	public class Activity1 : Activity
