3e32a9f30cddf0ab5439209d5d10831d53b57069
benohalloran <benohalloran@peter-imac.local>
2014-07-22 15:28:28 -0400

    [FragmentTransition] Update Readme
    Inline comments exactly match AOSP sample

--- FragmentTransition/FragmentTransition/MeatAdapter.cs
+++ FragmentTransition/FragmentTransition/MeatAdapter.cs
@@ -1,19 +1,4 @@
-﻿/*
- * Copyright 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-using System;
+﻿using System;
 
 using Android.Widget;
 using Android.App;
@@ -72,7 +57,7 @@ namespace FragmentTransition
 		}
 
 	}
-	//This allows for setting and retrieving of the view.Tag field
+	//Being a child of Java Object allows the ViewHolder to be set as a View's tag
 	public class ViewHolder : Java.Lang.Object
 	{
 		public ImageView image;
