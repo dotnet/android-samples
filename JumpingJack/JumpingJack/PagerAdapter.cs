6474868d1da7cdb33f945169b60020a90b5f0a4f
John Pilczak <john.pilczak@xamarin.com>
2014-07-29 11:37:55 -0400

    Add JumpingJack Wearable sample

---
+++ JumpingJack/JumpingJack/PagerAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Support.V13.App;
+
+using Java.Util;
+
+namespace JumpingJack
+{
+	public class PagerAdapter : FragmentPagerAdapter
+	{
+		List<Fragment> fragments = null;
+		public PagerAdapter (FragmentManager fm) : base(fm)
+		{
+			fragments = new List<Fragment> ();
+		}
+
+		public override Fragment GetItem (int position)
+		{
+			return fragments [position];
+		}
+
+		public override int Count {
+			get {
+				return fragments.Count;
+			}
+		}
+
+		public void AddFragment(Fragment fragment)
+		{
+			fragments.Add (fragment);
+			NotifyDataSetChanged ();
+		}
+	}
+}
+
