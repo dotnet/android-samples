abd34846e110303e160de853a732d9217bc84bb2
benohalloran <benohalloran@peter-imac.local>
2014-07-30 15:46:27 -0400

    [GestureBuilder] Save Renamed Gestures

--- GestureBuilder/GestureBuilder/GestureBuilderActivity.cs
+++ GestureBuilder/GestureBuilder/GestureBuilderActivity.cs
@@ -298,6 +298,7 @@ namespace GestureBuilder
 						sStore.RemoveGesture (gesture.Name, gesture.Gesture);
 						gesture.Name = mInput.Text.ToString ();
 						sStore.AddGesture (gesture.Name, gesture.Gesture);
+						sStore.Save ();
 						break;
 					}
 				}
