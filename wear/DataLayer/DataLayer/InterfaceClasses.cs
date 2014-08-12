f10d6fb855be0be7897fff8d087678057c32d446
John Pilczak <john.pilczak@xamarin.com>
2014-08-12 15:26:12 -0400

    [DataLayer] Add to wear folder in master

---
+++ wear/DataLayer/DataLayer/InterfaceClasses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Android.Gms.Common.Apis;
+
+namespace DataLayer
+{
+	public class InterfaceClasses
+	{
+		public InterfaceClasses ()
+		{
+		}
+	}
+
+	public class ResultCallback : Java.Lang.Object, IResultCallback
+	{
+		public Action<Java.Lang.Object> OnResultAction;
+		public void OnResult (Java.Lang.Object result)
+		{
+			if (OnResultAction != null)
+				OnResultAction (result);
+		}
+	}
+}
+
