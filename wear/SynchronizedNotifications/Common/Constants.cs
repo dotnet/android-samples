0b51d1791d41b5738e42e6717cd78e33f2ced912
John Pilczak <john.pilczak@xamarin.com>
2014-08-13 17:08:27 -0400

    [SynchronizedNotifications] Add to wear folder

---
+++ wear/SynchronizedNotifications/Common/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Common
+{
+	public static class Constants
+	{
+		public const int WatchOnlyId = 2;
+		public const int PhoneOnlyId = 3;
+		public const int BothId = 4;
+
+		public const String BothPath = "/both";
+		public const String WatchOnlyPath = "/watch-only";
+		public const String KeyNotificationId = "notification-id";
+		public const String KeyTitle = "title";
+		public const String KeyContent = "content";
+
+		public const String ActionDismiss = "com.example.android.wearable.synchronizednotifications.DISMISS";
+	}
+}
+
