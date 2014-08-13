6d3f9142d0bdf85697fcfa9be14c74bab3c6b720
John Pilczak <john.pilczak@xamarin.com>
2014-08-13 17:55:02 -0400

    [WatchViewStub] Add to wear folder

---
+++ wear/WatchViewStub/README.md
@@ -0,0 +1,9 @@
+WatchViewStubSample
+===================
+This wearable-only sample demonstrate using the WatchViewStub control which will automatically select a layout suited for round or rectangular screens. Take a look at the 'Resources/layout/main_actvity.xml' layout file to see how this is done.
+
+Build and Deploy
+================
+To build this project, simply open it in the latest version of Xamarin Studio or Xamarin for Visual Studio, and build from the 'Build' application menu.
+
+This project will run on both the Android Wear emulator and a physical Android Wear device.
---
+++ wear/WatchViewStub/WatchViewStub/MainActivity.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Wearable.Views;
+using Android.Support.V4.View;
+using Java.Interop;
+using Android.Views.Animations;
+
+namespace WatchViewStubSample
+{
+	[Activity (Label = "WatchViewStub", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity, Android.Support.Wearable.Views.WatchViewStub.IOnLayoutInflatedListener
+	{
+		RelativeLayout rectBackground, roundBackground;
+
+		GestureDetectorCompat gestureDetector;
+		DismissOverlayView dismissOverlayView;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.main_activity);
+
+			WatchViewStub stub = (WatchViewStub)FindViewById (Resource.Id.stub);
+			stub.SetOnLayoutInflatedListener (this);
+
+			dismissOverlayView = (DismissOverlayView)FindViewById (Resource.Id.dismiss_overlay);
+			gestureDetector = new GestureDetectorCompat (this, new LongPressListener (this));
+		}
+
+		public void OnLayoutInflated (WatchViewStub stub)
+		{
+			rectBackground = (RelativeLayout)FindViewById (Resource.Id.rect_layout);
+			roundBackground = (RelativeLayout)FindViewById (Resource.Id.round_layout);
+		}
+
+		/// <summary>
+		/// Animates the layout when clicked. The animation depends on whether the device is round or rectangular.
+		/// </summary>
+		/// <param name="view">View.</param>
+		[Export("onLayoutClicked")]
+		public void OnLayoutClicked(View view)
+		{
+			if (rectBackground != null) {
+				ScaleAnimation scaleAnimation = new ScaleAnimation (1, 0.7f, 1, 0.7f, Dimension.RelativeToSelf, 0.5f, Dimension.RelativeToSelf, 0.5f);
+				scaleAnimation.Duration = 300;
+				scaleAnimation.RepeatCount = 1;
+				scaleAnimation.RepeatMode = RepeatMode.Reverse;
+				rectBackground.StartAnimation (scaleAnimation);
+			}
+			if (roundBackground != null) {
+				roundBackground.Animate ().RotationBy (360).SetDuration (300).Start ();
+			}
+		}
+
+		public override bool DispatchTouchEvent (MotionEvent ev)
+		{
+			return gestureDetector.OnTouchEvent (ev) || base.DispatchTouchEvent (ev);
+		}
+
+		private class LongPressListener : GestureDetector.SimpleOnGestureListener {
+			MainActivity activity;
+			public LongPressListener(MainActivity activity)
+			{
+				this.activity = activity;
+			}
+			public override void OnLongPress (MotionEvent e)
+			{
+				base.OnLongPress (e);
+				if (activity != null) {
+					activity.dismissOverlayView.Show ();
+				}
+			}
+		}
+	}
+
+
+}
+
+
