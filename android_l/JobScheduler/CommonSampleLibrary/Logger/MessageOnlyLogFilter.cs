cfe28fa68caff66ababb9cf9c4312d664b81f92b
benohalloran <benjamin.ohalloran@xamarin.com>
2014-08-19 13:53:41 -0400

    [Android L] Add Job Scheduler Sample

---
+++ android_l/JobScheduler/CommonSampleLibrary/Logger/MessageOnlyLogFilter.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+
+namespace CommonSampleLibrary
+{
+	/**
+ 	* Simple ILogNode filter, removes everything except the message.
+ 	* Useful for situations like on-screen log output where you don't want a lot of metadata displayed,
+ 	* just easy-to-read message updates as they're happening.
+ 	*/
+	public class MessageOnlyLogFilter : ILogNode
+	{
+		ILogNode mNext;
+
+		/**
+     	* Gets the next LogNode in the chain.
+     	* Sets the LogNode data will be sent to..
+     	*/
+		public ILogNode NextNode {
+			get { return mNext; }
+			set { mNext = value; }
+		}
+
+		/**
+     	* Takes the "next" LogNode as a parameter, to simplify chaining.
+     	*/
+		public MessageOnlyLogFilter (ILogNode next)
+		{
+			mNext = next;
+		}
+
+		public MessageOnlyLogFilter ()
+		{
+		}
+
+		public void WriteLine (LogPriority priority, string tag, string msg, Java.Lang.Throwable tr)
+		{
+			if (NextNode != null) {
+				NextNode.WriteLine (LogPriority.Info, null, msg, null);
+			}
+		}
+	}
+}
+
---
+++ android_l/JobScheduler/README.md
@@ -0,0 +1,23 @@
+Job Scheduler
+=============
+This sample demonstrates the new JobScheduler API. The JobScheduler API is part of Android-L's 'Project Volta' which provides new APIs to increase battery life.
+
+JobScheduler allows applications to request non-user facing tasks to be run by the Android system in such a way as to maximize battery life. 
+
+This sample doesn't do any actual 'work', but demonstrates how to use the API.
+
+Instructions
+------------
+* Select any parameters from the main screen to restrict when the job can run.
+* Set a small deadline time. Android will automatically run the job after the deadline has passed.
+* Hit 'Schedule Job'
+* The job will begin running no latter than the deadline. When the job begins, onStartTask will light up green.
+* When the job terminates, onStopTask will light up red.
+Build Requirements
+------------------
+Download the latest version of Xamarin Studio. Open JobScheduler.sln in Xamarin Studio and run the project. This project requires the API level 21 (L) SDK platform.
+
+Author
+------
+Copyright (c) 2005-2008, The Android Open Source Project  
+Ported to Xamarin.Android by Ben O'Halloran
