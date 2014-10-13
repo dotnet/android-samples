79f9a077757a4a9edd38a98a2098576685f007b6
Victor Chelaru <VicChelaru@gmail.com>
2014-10-13 10:22:05 -0600

    Added complete project.

---
+++ BouncingGameCompleteAndroid/BouncingGame/GameAppDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CocosSharp;
+
+namespace BouncingGame
+{
+	public class GameAppDelegate : CCApplicationDelegate
+	{
+		public override void ApplicationDidFinishLaunching (CCApplication application, CCWindow mainWindow)
+		{
+			application.PreferMultiSampling = false;
+			application.ContentRootDirectory = "Content";
+
+			mainWindow.SupportedDisplayOrientations = CCDisplayOrientation.Portrait;
+
+			GameScene gameScene = new GameScene (mainWindow);
+			mainWindow.RunWithScene (gameScene);
+
+		}
+
+		public override void ApplicationDidEnterBackground (CCApplication application)
+		{
+		}
+
+		public override void ApplicationWillEnterForeground (CCApplication application)
+		{
+		}
+	}
+}
