c56025740aae4c6105a23f9cdb1580f33c822846
John Pilczak <john.pilczak@xamarin.com>
2014-11-11 12:38:49 -0500

    [VisualGameController] Rename directories

--- android_tv/VisualGameController/VisualGameController/Properties/AssemblyInfo.cs
+++ android_tv/VisualGameController/VisualGameController/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using Android.App;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("Visual-Game-Controller")]
+[assembly: AssemblyTitle ("VisualGameController")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
