24eb47f84af46038b75b0e38cf0486cfe62643ba
Mark McLemore <mgmclemore@gmail.com>
2014-12-10 12:17:04 -0800

    Convert WeatherREST to use an async delegate.

--- WeatherREST/README.md
+++ WeatherREST/README.md
@@ -4,7 +4,7 @@ WeatherREST
 This sample app accompanies the article, 
 [Call a REST Web Service](http://developer.xamarin.com/recipes/android/web_services/consuming_services/call_a_rest_web_service/).
 You can use this sample to learn how to call a RESTful service via 
-HTTP and parse the JSON results. 
+HTTP and handle/parse the JSON results asynchronously. 
 
 This example app sends latitude and longitude information in an HTTP 
 request (to *Geonames.org*) and gets back weather information for that 
@@ -17,8 +17,13 @@ display:
 ![](Screenshots/example-screen.png)
 
 This example uses `System.Json` to parse the results of the HTTP 
-request. To keep this example simple and focused on REST, this code 
-does not use asynchronous calls to the server. 
+request. Parsing and display are handled asynchronously in a
+callback delegate. 
+
+Note that this sample uses a geonames.org demo account, which supports 
+a limited number of accesses. If the display is not reporting weather 
+information, check your IDE output window to view the HTTP response and 
+see if it contains a "daily limit has been exceeded" message. 
 
 You will need an Android device or emulator with a large screen (such as 
 a Nexus 5 or an AVD with the WXGA720 skin) to view the results. 
--- WeatherREST/WeatherREST/MainActivity.cs
+++ WeatherREST/WeatherREST/MainActivity.cs
@@ -28,18 +28,12 @@ namespace WeatherREST
             EditText longitude = FindViewById<EditText>(Resource.Id.longText);
             Button button = FindViewById<Button>(Resource.Id.GetWeatherButton);
 
-            // Get the weather reporting fields from the layout resource: 
-            TextView location = FindViewById<TextView>(Resource.Id.locationText);
-            TextView temperature = FindViewById<TextView>(Resource.Id.tempText);
-            TextView humidity = FindViewById<TextView>(Resource.Id.humidText);
-            TextView conditions = FindViewById<TextView>(Resource.Id.condText);
-
-            // When the user clicks the button, use REST to send the query to geonames,
-            // the display the result.
+            // When the user clicks the button, send the REST request to geonames.org,
             button.Click += delegate
             {
-                // Get the user's entered latitude and longitude and create a query. Note
-                // that input error checking is ignored here.
+                // Get the latitude and longitude entered by the user and create a query. 
+                // Note that input error checking is ignored here.
+
                 string url = "http://api.geonames.org/findNearByWeatherJSON?lat=" +
                              latitude.Text +
                              "&lng=" +
@@ -51,62 +45,70 @@ namespace WeatherREST
                 request.ContentType = "application/json";
                 request.Method = "GET";
 
-                // Wait for a response from the server. Note that, in a production app,
-                // you would implement this to be an asychronous wait (so the UI isn't
-                // frozen during the wait). To keep the REST example straightforward, 
-                // this sample app waits synchronously for the HTTP response before
-                // updating the screen.
-
-                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-                {
-                    // If there is an error, print out why.
-                    if (response.StatusCode != HttpStatusCode.OK)
-                        Console.Out.WriteLine("Error fetching weather data. Server returned status code: {0}", response.StatusCode);
-
-                    // Get a stream representation of the HTTP web response:
-                    using (Stream stream = response.GetResponseStream())
-                    {
-                        // Use this stream to build a JSON object:
-                        JsonObject jsonDoc = (JsonObject)JsonObject.Load(stream);
-
-                        // Extract the value for the field name "weatherObservation":
-                        JsonValue results = jsonDoc["weatherObservation"];
-
-                        //.......................................................................
-                        // The remainder of this example formats the resulting weather
-                        // information to the lower half of the screen.
-                        
-                        // The resulting value is an array of name/value pairs. First, extract
-                        // the "stationName" (location string), and fill the location TextBox:
-                        location.Text = results["stationName"];
-
-                        // The temperature is expressed in Celsius:
-                        double temp = results["temperature"];
-                        // Convert to Fahrenheit:
-                        temp = ((9.0 / 5.0) * temp) + 32;
-
-                        // Write the temperature (one decimal place) into the temperature TextBox:
-                        temperature.Text = String.Format("{0:F1}", temp) + "° F";
-
-                        // Get the percent humidity and write it to the humidity TextBox:
-                        double humidPercent = results["humidity"];
-                        humidity.Text = humidPercent.ToString() + "%";
-
-                        // Get the "clouds" and "weatherConditions" strings and combine them.
-                        // Ignore strings that are reported as "n/a":
-                        string cloudy = results["clouds"];
-                        if (cloudy.Equals("n/a"))
-                            cloudy = "";
-
-                        string cond = results["weatherCondition"];
-                        if (cond.Equals("n/a"))
-                            cond = "";
-
-                        // Write the result to the conditions TextBox:
-                        conditions.Text = cloudy + " " + cond;
-                    }
-                }
+                // Begin the asynchronous request to the server. The callback delegate (MyCallback,
+                // below) handles the HTTP response.
+                IAsyncResult result = (IAsyncResult)request.BeginGetResponse(MyCallback, request);
             };
         }
+
+        // The async callback delegate: parse the JSON response and update the UI with results.
+        private void MyCallback(IAsyncResult result)
+        {
+            var request = (HttpWebRequest)result.AsyncState;
+            var response = request.EndGetResponse(result);
+
+            // Get a stream representation of the HTTP web response:
+            using (Stream stream = response.GetResponseStream())
+            {
+                // Use this stream to build a JSON object:
+                JsonObject jsonDoc = (JsonObject)JsonObject.Load(stream);
+                Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+
+                // Extract the array of name/value results for the field name "weatherObservation":
+                // Note that there is no exception handling for when this field is not found.
+                JsonValue weatherResults = jsonDoc["weatherObservation"];
+
+                // Update the screen while in the UI context:
+                RunOnUiThread(() =>
+                {
+                    //.......................................................................
+                    // The remainder of this example formats the resulting weather
+                    // information to the lower half of the screen.
+
+                    // Get the weather reporting fields from the layout resource: 
+                    TextView location = FindViewById<TextView>(Resource.Id.locationText);
+                    TextView temperature = FindViewById<TextView>(Resource.Id.tempText);
+                    TextView humidity = FindViewById<TextView>(Resource.Id.humidText);
+                    TextView conditions = FindViewById<TextView>(Resource.Id.condText);
+
+                    // Extract the "stationName" (location string) and write it to the location TextBox:
+                    location.Text = weatherResults["stationName"];
+
+                    // The temperature is expressed in Celsius:
+                    double temp = weatherResults["temperature"];
+                    // Convert to Fahrenheit:
+                    temp = ((9.0 / 5.0) * temp) + 32;
+                    // Write the temperature (one decimal place) to the temperature TextBox:
+                    temperature.Text = String.Format("{0:F1}", temp) + "° F";
+
+                    // Get the percent humidity and write it to the humidity TextBox:
+                    double humidPercent = weatherResults["humidity"];
+                    humidity.Text = humidPercent.ToString() + "%";
+
+                    // Get the "clouds" and "weatherConditions" strings and combine them.
+                    // Ignore strings that are reported as "n/a":
+                    string cloudy = weatherResults["clouds"];
+                    if (cloudy.Equals("n/a"))
+                        cloudy = "";
+                    string cond = weatherResults["weatherCondition"];
+                    if (cond.Equals("n/a"))
+                        cond = "";
+
+                    // Write the result to the conditions TextBox:
+                    conditions.Text = cloudy + " " + cond;
+                });
+            }
+        }
+
     }
 }
