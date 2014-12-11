b800831adb1ce48b25f602c31c4682e2efc28610
Mark McLemore <mgmclemore@gmail.com>
2014-12-11 11:20:31 -0800

    Convert WeatherREST to use async/await.

--- WeatherREST/WeatherREST/MainActivity.cs
+++ WeatherREST/WeatherREST/MainActivity.cs
@@ -8,6 +8,7 @@ using Android.OS;
 using System.Net;
 using System.IO;
 using System.Json;
+using System.Threading.Tasks;
 
 namespace WeatherREST
 {
@@ -29,8 +30,8 @@ namespace WeatherREST
             Button button = FindViewById<Button>(Resource.Id.GetWeatherButton);
 
             // When the user clicks the button, send the REST request to geonames.org,
-            button.Click += delegate
-            {
+            button.Click += async (sender, e) => {
+
                 // Get the latitude and longitude entered by the user and create a query. 
                 // Note that input error checking is ignored here.
 
@@ -40,75 +41,76 @@ namespace WeatherREST
                              longitude.Text +
                              "&username=demo";
 
-                // Create an HTTP web request using this URL:
-                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
-                request.ContentType = "application/json";
-                request.Method = "GET";
-
-                // Begin the asynchronous request to the server. The callback delegate (MyCallback,
-                // below) handles the HTTP response.
-                IAsyncResult result = (IAsyncResult)request.BeginGetResponse(MyCallback, request);
+                // Fetch the weather information asynchronously, then update the screen:
+                await FetchAndDisplayAsync(url);
             };
         }
 
-        // The async callback delegate: parse the JSON response and update the UI with results.
-        private void MyCallback(IAsyncResult result)
+        // Gets weather data from the passed URL, parses the results, then writes 
+        // temperature, humidity, conditions, and location to the screen.
+
+        private async Task FetchAndDisplayAsync(string url)
         {
-            var request = (HttpWebRequest)result.AsyncState;
-            var response = request.EndGetResponse(result);
+            // Create an HTTP web request using the URL:
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "GET";
 
-            // Get a stream representation of the HTTP web response:
-            using (Stream stream = response.GetResponseStream())
+            // Send the request to the server and wait for the response:
+            using (WebResponse response = await request.GetResponseAsync())
             {
-                // Use this stream to build a JSON object:
-                JsonObject jsonDoc = (JsonObject)JsonObject.Load(stream);
-                Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
-
-                // Extract the array of name/value results for the field name "weatherObservation":
-                // Note that there is no exception handling for when this field is not found.
-                JsonValue weatherResults = jsonDoc["weatherObservation"];
-
-                // Update the screen while in the UI context:
-                RunOnUiThread(() =>
+                // Get a stream representation of the HTTP web response:
+                using (Stream stream = response.GetResponseStream())
                 {
-                    //.......................................................................
-                    // The remainder of this example formats the resulting weather
-                    // information to the lower half of the screen.
+                    // Use this stream to build a JSON object:
+                    JsonValue jsonDoc = await Task.Run (() => JsonObject.Load(stream));
+                    Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
 
-                    // Get the weather reporting fields from the layout resource: 
-                    TextView location = FindViewById<TextView>(Resource.Id.locationText);
-                    TextView temperature = FindViewById<TextView>(Resource.Id.tempText);
-                    TextView humidity = FindViewById<TextView>(Resource.Id.humidText);
-                    TextView conditions = FindViewById<TextView>(Resource.Id.condText);
+                    // Extract the array of name/value results for the field name "weatherObservation":
+                    // Note that there is no exception handling for when this field is not found.
+                    JsonValue weatherResults = jsonDoc["weatherObservation"];
 
-                    // Extract the "stationName" (location string) and write it to the location TextBox:
-                    location.Text = weatherResults["stationName"];
+                    // Update the screen while in the UI context:
+                    RunOnUiThread(() =>
+                    {
+                        //.......................................................................
+                        // The remainder of this example formats the resulting weather
+                        // information to the lower half of the screen.
 
-                    // The temperature is expressed in Celsius:
-                    double temp = weatherResults["temperature"];
-                    // Convert to Fahrenheit:
-                    temp = ((9.0 / 5.0) * temp) + 32;
-                    // Write the temperature (one decimal place) to the temperature TextBox:
-                    temperature.Text = String.Format("{0:F1}", temp) + "° F";
+                        // Get the weather reporting fields from the layout resource: 
+                        TextView location = FindViewById<TextView>(Resource.Id.locationText);
+                        TextView temperature = FindViewById<TextView>(Resource.Id.tempText);
+                        TextView humidity = FindViewById<TextView>(Resource.Id.humidText);
+                        TextView conditions = FindViewById<TextView>(Resource.Id.condText);
 
-                    // Get the percent humidity and write it to the humidity TextBox:
-                    double humidPercent = weatherResults["humidity"];
-                    humidity.Text = humidPercent.ToString() + "%";
+                        // Extract the "stationName" (location string) and write it to the location TextBox:
+                        location.Text = weatherResults["stationName"];
 
-                    // Get the "clouds" and "weatherConditions" strings and combine them.
-                    // Ignore strings that are reported as "n/a":
-                    string cloudy = weatherResults["clouds"];
-                    if (cloudy.Equals("n/a"))
-                        cloudy = "";
-                    string cond = weatherResults["weatherCondition"];
-                    if (cond.Equals("n/a"))
-                        cond = "";
+                        // The temperature is expressed in Celsius:
+                        double temp = weatherResults["temperature"];
+                        // Convert to Fahrenheit:
+                        temp = ((9.0 / 5.0) * temp) + 32;
+                        // Write the temperature (one decimal place) to the temperature TextBox:
+                        temperature.Text = String.Format("{0:F1}", temp) + "° F";
 
-                    // Write the result to the conditions TextBox:
-                    conditions.Text = cloudy + " " + cond;
-                });
+                        // Get the percent humidity and write it to the humidity TextBox:
+                        double humidPercent = weatherResults["humidity"];
+                        humidity.Text = humidPercent.ToString() + "%";
+
+                        // Get the "clouds" and "weatherConditions" strings and combine them.
+                        // Ignore strings that are reported as "n/a":
+                        string cloudy = weatherResults["clouds"];
+                        if (cloudy.Equals("n/a"))
+                            cloudy = "";
+                        string cond = weatherResults["weatherCondition"];
+                        if (cond.Equals("n/a"))
+                            cond = "";
+
+                        // Write the result to the conditions TextBox:
+                        conditions.Text = cloudy + " " + cond;
+                    });
+                }
             }
         }
-
     }
 }
