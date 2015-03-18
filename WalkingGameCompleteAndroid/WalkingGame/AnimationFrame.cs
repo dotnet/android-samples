4f87018d228b38f38838e177f0a991350eb37007
Victor Chelaru <VicChelaru@gmail.com>
2015-03-18 16:05:41 -0600

    Fixed up WalkingGame complete to use MonoGame 3.3

--- WalkingGameCompleteAndroid/WalkingGame/AnimationFrame.cs
+++ WalkingGameCompleteAndroid/WalkingGame/AnimationFrame.cs
@@ -3,10 +3,9 @@ using Microsoft.Xna.Framework;
 
 namespace WalkingGame
 {
-	public class AnimationFrame
-	{
-		public Rectangle SourceRectangle { get; set; }
-		public TimeSpan Duration { get; set; }
-	}
+	    public class AnimationFrame
+	    {
+	        public Rectangle SourceRectangle { get; set; }
+	        public TimeSpan Duration { get; set; }
+	    }
 }
-
--- WalkingGameCompleteAndroid/WalkingGame/CharacterEntity.cs
+++ WalkingGameCompleteAndroid/WalkingGame/CharacterEntity.cs
@@ -19,7 +19,7 @@ namespace WalkingGame
 		Animation standLeft;
 		Animation standRight;
 
-		Animation currentAnimation;
+		Animation currentAnimation; 
 
 		public float X
 		{
@@ -36,122 +36,57 @@ namespace WalkingGame
 		public CharacterEntity (GraphicsDevice graphicsDevice)
 		{
 			if (characterSheetTexture == null)
-			{
-				using (var stream = TitleContainer.OpenStream ("Content/charactersheet.png"))
-				{
-					characterSheetTexture = Texture2D.FromStream (graphicsDevice, stream);
-				}
-			}
+				    {
+				        using (var stream = TitleContainer.OpenStream ("Content/charactersheet.png"))
+					        {
+					            characterSheetTexture = Texture2D.FromStream (graphicsDevice, stream);
+					        }
+				    }
 
-			walkDown = new Animation ();
-			walkDown.AddFrame (new Rectangle (0, 0, 16, 16), TimeSpan.FromSeconds (.25));
-			walkDown.AddFrame (new Rectangle (16, 0, 16, 16), TimeSpan.FromSeconds (.25));
-			walkDown.AddFrame (new Rectangle (0, 0, 16, 16), TimeSpan.FromSeconds (.25));
-			walkDown.AddFrame (new Rectangle (32, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkDown = new Animation ();
+			    walkDown.AddFrame (new Rectangle (0, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkDown.AddFrame (new Rectangle (16, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkDown.AddFrame (new Rectangle (0, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkDown.AddFrame (new Rectangle (32, 0, 16, 16), TimeSpan.FromSeconds (.25));
 
-			walkUp = new Animation ();
-			walkUp.AddFrame (new Rectangle (144, 0, 16, 16), TimeSpan.FromSeconds (.25));
-			walkUp.AddFrame (new Rectangle (160, 0, 16, 16), TimeSpan.FromSeconds (.25));
-			walkUp.AddFrame (new Rectangle (144, 0, 16, 16), TimeSpan.FromSeconds (.25));
-			walkUp.AddFrame (new Rectangle (176, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkUp = new Animation ();
+			    walkUp.AddFrame (new Rectangle (144, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkUp.AddFrame (new Rectangle (160, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkUp.AddFrame (new Rectangle (144, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkUp.AddFrame (new Rectangle (176, 0, 16, 16), TimeSpan.FromSeconds (.25));
 
-			walkLeft = new Animation ();
-			walkLeft.AddFrame (new Rectangle (48, 0, 16, 16), TimeSpan.FromSeconds (.25));
-			walkLeft.AddFrame (new Rectangle (64, 0, 16, 16), TimeSpan.FromSeconds (.25));
-			walkLeft.AddFrame (new Rectangle (48, 0, 16, 16), TimeSpan.FromSeconds (.25));
-			walkLeft.AddFrame (new Rectangle (80, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkLeft = new Animation ();
+			    walkLeft.AddFrame (new Rectangle (48, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkLeft.AddFrame (new Rectangle (64, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkLeft.AddFrame (new Rectangle (48, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkLeft.AddFrame (new Rectangle (80, 0, 16, 16), TimeSpan.FromSeconds (.25));
 
-			walkRight = new Animation ();
-			walkRight.AddFrame (new Rectangle (96, 0, 16, 16), TimeSpan.FromSeconds (.25));
-			walkRight.AddFrame (new Rectangle (112, 0, 16, 16), TimeSpan.FromSeconds (.25));
-			walkRight.AddFrame (new Rectangle (96, 0, 16, 16), TimeSpan.FromSeconds (.25));
-			walkRight.AddFrame (new Rectangle (128, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkRight = new Animation ();
+			    walkRight.AddFrame (new Rectangle (96, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkRight.AddFrame (new Rectangle (112, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkRight.AddFrame (new Rectangle (96, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    walkRight.AddFrame (new Rectangle (128, 0, 16, 16), TimeSpan.FromSeconds (.25));
 
-			// Standing animations only have a single frame of animation:
-			standDown = new Animation ();
-			standDown.AddFrame (new Rectangle (0, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    // Standing animations only have a single frame of animation:
+			    standDown = new Animation ();
+			    standDown.AddFrame (new Rectangle (0, 0, 16, 16), TimeSpan.FromSeconds (.25));
 
-			standUp = new Animation ();
-			standUp.AddFrame (new Rectangle (144, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    standUp = new Animation ();
+			    standUp.AddFrame (new Rectangle (144, 0, 16, 16), TimeSpan.FromSeconds (.25));
 
-			standLeft = new Animation ();
-			standLeft.AddFrame (new Rectangle (48, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    standLeft = new Animation ();
+			    standLeft.AddFrame (new Rectangle (48, 0, 16, 16), TimeSpan.FromSeconds (.25));
 
-			standRight = new Animation ();
-			standRight.AddFrame (new Rectangle (96, 0, 16, 16), TimeSpan.FromSeconds (.25));
+			    standRight = new Animation ();
+			    standRight.AddFrame (new Rectangle (96, 0, 16, 16), TimeSpan.FromSeconds (.25));
+
 		}
 
-public void Update(GameTime gameTime)
-{
-	var velocity = GetDesiredVelocityFromInput ();
-
-	this.X += velocity.X * (float)gameTime.ElapsedGameTime.TotalSeconds;
-	this.Y += velocity.Y * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-
-	if (velocity != Vector2.Zero)
-	{
-		bool movingHorizontally = Math.Abs (velocity.X) > Math.Abs (velocity.Y);
-		if (movingHorizontally)
-		{
-			if (velocity.X > 0)
-			{
-				currentAnimation = walkRight;
-			}
-			else
-			{
-				currentAnimation = walkLeft;
-			}
-		}
-		else
-		{
-			if (velocity.Y > 0)
-			{
-				currentAnimation = walkDown;
-			}
-			else
-			{
-				currentAnimation = walkUp;
-			}
-		}
-	}
-	else
-	{
-		// If the character was walking, we can set the standing animation
-		// according to the walking animation that is playing:
-		if (currentAnimation == walkRight)
-		{
-			currentAnimation = standRight;
-		}
-		else if (currentAnimation == walkLeft)
-		{
-			currentAnimation = standLeft;
-		}
-		else if (currentAnimation == walkUp)
-		{
-			currentAnimation = standUp;
-		}
-		else if (currentAnimation == walkDown)
-		{
-			currentAnimation = standDown;
-		}
-		else if (currentAnimation == null)
-		{
-			currentAnimation = standDown;
-		}
-
-		// if none of the above code hit then the character
-		// is already standing, so no need to change the animation.
-	}
-
-	currentAnimation.Update (gameTime);
-}
-
-		Vector2 GetDesiredVelocityFromInput()
+		Vector2 GetDesiredVelocityFromInput ()
 		{
 			Vector2 desiredVelocity = new Vector2 ();
 
-			TouchCollection touchCollection = TouchPanel.GetState();
+			TouchCollection touchCollection = TouchPanel.GetState ();
 
 			if (touchCollection.Count > 0)
 			{
@@ -160,7 +95,7 @@ public void Update(GameTime gameTime)
 
 				if (desiredVelocity.X != 0 || desiredVelocity.Y != 0)
 				{
-					desiredVelocity.Normalize();
+					desiredVelocity.Normalize ();
 					const float desiredSpeed = 200;
 					desiredVelocity *= desiredSpeed;
 				}
@@ -169,14 +104,100 @@ public void Update(GameTime gameTime)
 			return desiredVelocity;
 		}
 
-		public void Draw(SpriteBatch spriteBatch)
+
+		public void Update (GameTime gameTime)
+		{
+			var velocity = GetDesiredVelocityFromInput ();
+
+			this.X += velocity.X * (float)gameTime.ElapsedGameTime.TotalSeconds;
+			this.Y += velocity.Y * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+			// We can use the velocity variable to determine if the 
+			// character is moving or standing still
+			bool isMoving = velocity != Vector2.Zero;
+			if (isMoving)
+			{
+				// If the absolute value of the X component
+				// is larger than the absolute value of the Y
+				// component, then that means the character is
+				// moving horizontally:
+				bool isMovingHorizontally = Math.Abs (velocity.X) > Math.Abs (velocity.Y);
+				if (isMovingHorizontally)
+				{
+					// No that we know the character is moving horizontally 
+					// we can check if the velocity is positive (moving right)
+					// or negative (moving left)
+					if (velocity.X > 0)
+					{
+						currentAnimation = walkRight;
+					}
+					else
+					{
+						currentAnimation = walkLeft;
+					}
+				}
+				else
+				{
+					// If the character is not moving horizontally
+					// then it must be moving vertically. The SpriteBatch
+					// class treats positive Y as down, so this defines the
+					// coordinate system for our game. Therefore if
+					// Y is positive then the character is moving down.
+					// Otherwise, the character is moving up.
+					if (velocity.Y > 0)
+					{
+						currentAnimation = walkDown;
+					}
+					else
+					{
+						currentAnimation = walkUp;
+					}
+				}
+			}
+			else
+			{
+				// This else statement contains logic for if the
+				// character is standing still.
+				// First we are going to check if the character
+				// is currently playing any walking animations.
+				// If so, then we want to switch to a standing animation.
+				// We want to preserve the direction that the character
+				// is facing so we'll set the corresponding standing
+				// animation according to the walking animation being played.
+				if (currentAnimation == walkRight)
+				{
+					currentAnimation = standRight;
+				}
+				else if (currentAnimation == walkLeft)
+				{
+					currentAnimation = standLeft;
+				}
+				else if (currentAnimation == walkUp)
+				{
+					currentAnimation = standUp;
+				}
+				else if (currentAnimation == walkDown)
+				{
+					currentAnimation = standDown;
+				}
+				// If the character is standing still but is not showing
+				// any animation at all then we'll default to facing down.
+				else if (currentAnimation == null)
+				{
+					currentAnimation = standDown;
+				}
+			}
+
+			currentAnimation.Update (gameTime);
+		}
+
+		public void Draw (SpriteBatch spriteBatch)
 		{
 			Vector2 topLeftOfSprite = new Vector2 (this.X, this.Y);
 			Color tintColor = Color.White;
 			var sourceRectangle = currentAnimation.CurrentRectangle;
 
-			spriteBatch.Draw(characterSheetTexture, topLeftOfSprite, sourceRectangle, Color.White);
+			spriteBatch.Draw (characterSheetTexture, topLeftOfSprite, sourceRectangle, Color.White);
 		}
 	}
 }
-
