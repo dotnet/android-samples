319336f0878ec9f8759b93720fe0001b71ed7249
Redth <jondick@gmail.com>
2015-06-01 14:05:27 -0400

    [cheesesquare] Ported Chris Banes' Cheesesquare sample to C# This demonstrates many uses of the new design support library as well as RecyclerView, and CardView and Support V4

---
+++ Cheesesquare/CheeseDetailActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.Design.Widget;
+using Android.App;
+
+namespace Cheesesquare
+{
+    [Activity (Label="Details")]
+    public class CheeseDetailActivity : AppCompatActivity
+    {
+        public const string EXTRA_NAME = "cheese_name";
+
+        protected override void OnCreate (Bundle savedInstanceState) 
+        {
+            base.OnCreate (savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_detail);
+
+            var cheeseName = Intent.GetStringExtra (EXTRA_NAME);
+
+            var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar (toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled (true);
+
+            var collapsingToolbar = FindViewById<CollapsingToolbarLayout> (Resource.Id.collapsing_toolbar);
+            collapsingToolbar.SetTitle (cheeseName);
+
+            loadBackdrop();
+        }
+            
+        public override bool OnOptionsItemSelected (IMenuItem item) 
+        {
+            switch (item.ItemId) {
+            case Android.Resource.Id.Home:
+                Finish ();
+                return true;
+            }
+            return base.OnOptionsItemSelected (item);
+        }
+
+        void loadBackdrop() 
+        {            
+            var imageView = FindViewById<ImageView> (Resource.Id.backdrop);
+
+            var r = Cheeses.GetRandomCheeseBackground ();
+            imageView.SetImageResource (r);
+        }
+            
+        public override bool OnCreateOptionsMenu(IMenu menu) 
+        {
+            MenuInflater.Inflate (Resource.Menu.sample_actions, menu);
+            return true;
+        }
+    }
+}
+
