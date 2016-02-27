1b5073fa79feb13e626822355a520d149822fa6f
olegoid <olegoid91@gmail.com>
2016-02-26 23:23:28 -0300

    [HowsMyTls] Add TLS demonstration sample

---
+++ HowsMyTls/HowsMyTls/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace HowsMyTls {
+	public static class Extensions {
+
+		public static byte[] ReadAllBytes (this Stream input)
+		{
+			byte[] buffer = new byte [16 * 1024];
+			using (var ms = new MemoryStream ()) {
+				int read;
+
+				while ((read = input.Read (buffer, 0, buffer.Length)) > 0)
+					ms.Write (buffer, 0, read);
+
+				return ms.ToArray ();
+			}
+		}
+	}
+}
+
---
+++ HowsMyTls/HowsMyTls/Model/ClientStatus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace HowsMyTls {
+	public class ClientStatus {
+
+		[JsonProperty ("given_cipher_suites")]
+		public List<string> CypherSuites { get; set; }
+
+		[JsonProperty("ephemeral_keys_supported")]
+		public bool EphemeralKeysSupported { get; set; }
+
+		[JsonProperty ("session_ticket_supported")]
+		public bool SessionTicketSupported { get; set; }
+
+		[JsonProperty ("tls_compression_supported")]
+		public bool TlsCompressionSupported { get; set; }
+
+		[JsonProperty ("unknown_cipher_suite_supported")]
+		public bool UnknownCipherSuiteSupported { get; set; }
+
+		[JsonProperty ("beast_vuln")]
+		public bool BeastVuln { get; set; }
+
+		[JsonProperty ("able_to_detect_n_minus_one_splitting")]
+		public bool AbleToDetectNMinusOneSplitting { get; set; }
+
+		[JsonProperty ("tls_version")]
+		public string TlsVersion { get; set; }
+
+		[JsonProperty ("rating")]
+		public string Rating { get; set; }
+	}
+}
+
