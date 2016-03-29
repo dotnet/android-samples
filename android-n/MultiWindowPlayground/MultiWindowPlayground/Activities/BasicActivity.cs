1bda85d5965e6da3713e1357dfc5ce6ef9661617
Kyle White <kyle@xamarin.com>
2016-03-29 19:00:51 -0400

    [Android N] Adding MultiWindowPlayground sample

---
+++ android-n/MultiWindowPlayground/MultiWindowPlayground/Activities/BasicActivity.cs
@@ -0,0 +1,22 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace MultiWindowPlayground
+{
+	[Activity(LaunchMode = LaunchMode.SingleInstance, TaskAffinity = "")]
+	public class BasicActivity : LoggingActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.activity_logging);
+
+			// Set the color and description
+			SetDescription(Resource.String.activity_description_basic);
+			SetBackgroundColor(Resource.Color.gray);
+		}
+	}
+}
+
---
+++ android-n/MultiWindowPlayground/MultiWindowPlayground/Activities/CustomConfigurationChangeActivity.cs
@@ -0,0 +1,32 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace MultiWindowPlayground
+{
+	[Activity(ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.SmallestScreenSize | ConfigChanges.ScreenLayout | ConfigChanges.Orientation,
+	          LaunchMode = LaunchMode.SingleInstance, TaskAffinity = "")]
+	public class CustomConfigurationChangeActivity : LoggingActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.activity_logging);
+
+			// Create your application here
+			SetDescription(Resource.String.activity_custom_description);
+			SetBackgroundColor(Resource.Color.cyan);
+		}
+
+		public override void OnConfigurationChanged(Android.Content.Res.Configuration newConfig)
+		{
+			base.OnConfigurationChanged(newConfig);
+
+			// Note: The implementation in LoggingActivity logs the output to the new configuration.
+			// This callback is received whenever the configuration is updated, for example when the
+			// size of this Activity is changed.
+		}
+	}
+}
+
