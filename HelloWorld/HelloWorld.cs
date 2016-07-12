855682e537748086aed726463cb0c28d96d22992
Peter Collins <peter.collins@xamarin.com>
2016-07-11 22:07:58 -0400

    [HelloWorld] Update for most recent lint checks

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,11 +1,20 @@
 using System;
 
 using Android.App;
+using Android.Content;
 using Android.OS;
 
 namespace Mono.Samples.HelloWorld
 {
 	[Activity (Label = "Hello World Demo", MainLauncher = true)]
+	[IntentFilter(new[] { Intent.ActionView },
+		Categories = new[] {
+			Intent.CategoryDefault,
+			Intent.CategoryBrowsable
+		},
+		DataScheme = "http",
+		DataHost = "example.com"
+	)]
 	public class HelloAndroid : Activity
 	{
 		protected override void OnCreate (Bundle savedInstanceState)
