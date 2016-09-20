6e4a722d4ee9e78ca3213f86a461097ca1da37b7
Gonzalo Martin <gonzalo.martin@globallogic.com>
2016-09-20 14:47:37 -0300

    DirectReply implemented

---
+++ DirectReply/DirectReply/MainActivity.cs
@@ -0,0 +1,106 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Support.V4.App;
+using Android.Graphics;
+
+namespace DirectReply
+{
+	[Activity(Label = "DirectReply", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int requestCode = 0;
+
+		public const string REPLY_ACTION = "com.xamarin.directreply.REPLY";
+		public const string KEY_TEXT_REPLY = "key_text_reply";
+		public const string REQUEST_CODE_KEY = "request_code";
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			var button = FindViewById<Button>(Resource.Id.myButton);
+
+            // Set button listener
+		    button.Click += (sender, args) =>
+		    {
+                OnButtonClick();
+            };
+		}
+
+	    private void OnButtonClick()
+	    {
+            requestCode++;
+
+            // Build PendingIntent
+	        var pendingIntent = MakePendingIntent();
+
+            var replyText = GetString(Resource.String.reply_text);
+
+            // Create remote input that will read text
+            var remoteInput = new Android.Support.V4.App.RemoteInput.Builder(KEY_TEXT_REPLY)
+                                         .SetLabel(replyText)
+                                         .Build();
+
+            // Build action for noticiation
+            var action = new NotificationCompat.Action.Builder(Resource.Drawable.action_reply, replyText, pendingIntent)
+                                               .AddRemoteInput(remoteInput)
+                                               .Build();
+
+            // Build notification
+            var notification = new NotificationCompat.Builder(this)
+                                                     .SetSmallIcon(Resource.Drawable.reply)
+                                                     .SetLargeIcon(BitmapFactory.DecodeResource(Resources, Resource.Drawable.avatar))
+                                                     .SetContentText("Hey, it is James! What's up?")
+                                                     .SetContentTitle(GetString(Resource.String.message))
+                                                     .SetAutoCancel(true)
+                                                     .AddAction(action)
+                                                     .Build();
+
+            // Notify
+            using (var notificationManager = NotificationManagerCompat.From(this))
+            {
+                notificationManager.Notify(requestCode, notification);
+            }
+        }
+
+	    private PendingIntent MakePendingIntent()
+	    {
+            PendingIntent pendingIntent = null;
+
+            if ((int)Build.VERSION.SdkInt >= (int)BuildVersionCodes.N)
+            {
+                // >= Android N
+                var intent = new Intent(REPLY_ACTION)
+                    .AddFlags(ActivityFlags.IncludeStoppedPackages)
+                    .SetAction(REPLY_ACTION)
+                    .PutExtra(REQUEST_CODE_KEY, requestCode);
+
+                pendingIntent = PendingIntent.GetBroadcast(this,
+                                                           requestCode,
+                                                           intent,
+                                                           PendingIntentFlags.UpdateCurrent);
+            }
+            else
+            {
+                // < Android N
+                var intent = new Intent(this, typeof(MainActivity));
+                intent.AddFlags(ActivityFlags.ClearTop | ActivityFlags.NewTask);
+
+                pendingIntent = PendingIntent.GetActivity(this,
+                                                          requestCode,
+                                                          intent,
+                                                          PendingIntentFlags.UpdateCurrent);
+            }
+
+	        return pendingIntent;
+	    }
+	}
+}
+
---
+++ DirectReply/DirectReply/MessageReplyReceiver.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Support.V4.App;
+using Android.Widget;
+using RemoteInput = Android.Support.V4.App.RemoteInput;
+
+namespace DirectReply
+{
+	[BroadcastReceiver(Enabled = true)]
+	[Android.App.IntentFilter(new[] { MainActivity.REPLY_ACTION })]
+	/// <summary>
+	/// A receiver that gets called when a reply is sent to a given conversationId
+	/// </summary>
+	public class MessageReplyReceiver : BroadcastReceiver
+	{
+		public override void OnReceive(Context context, Intent intent)
+		{
+			if (!MainActivity.REPLY_ACTION.Equals(intent.Action))
+				return;
+			
+
+			var requestId = intent.GetIntExtra(MainActivity.REQUEST_CODE_KEY, -1);
+			if (requestId == -1)
+				return;
+
+			var reply = GetMessageText(intent);
+			using (var notificationManager = NotificationManagerCompat.From(context))
+			{
+				// Create new notification to display, or re-build existing conversation to update with new response
+				var notificationBuilder = new NotificationCompat.Builder(context);
+				notificationBuilder.SetSmallIcon(Resource.Drawable.reply);
+				notificationBuilder.SetContentText(Application.Context.GetString(Resource.String.replied));
+				var repliedNotification = notificationBuilder.Build();
+
+
+				// Call notify to stop progress spinner. 
+				notificationManager.Notify(requestId, repliedNotification);
+			}
+
+			Toast.MakeText(context, $"Message sent: {reply}", ToastLength.Long).Show();
+		}
+
+		/// <summary>
+		/// Get the message text from the intent.
+		/// Note that you should call <see cref="Android.Support.V4.App.RemoteInput.GetResultsFromIntent(intent)"/> 
+		/// to process the RemoteInput.
+		/// </summary>
+		/// <returns>The message text.</returns>
+		/// <param name="intent">Intent.</param>
+		static string GetMessageText(Intent intent)
+		{
+			var remoteInput = RemoteInput.GetResultsFromIntent(intent);
+			return remoteInput != null ? remoteInput.GetCharSequence(MainActivity.KEY_TEXT_REPLY) : string.Empty;
+		}
+	}
+}
