f89ae066e233d67a8e2923a0e18712465a696696
Gonzalo Martin <gonzalo.martin@globallogic.com>
2016-09-29 12:01:14 -0300

    Fix landscape mode

---
+++ ActionBarViewPager/HelloSwipeViewWithTabs/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V4.View;
+using Android.Support.V4.App;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+
+namespace HelloSwipeViewWithTabs
+{
+    [Activity(Label = "HelloSwipeViewWithTabs", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.main);
+
+            // Find views
+            var pager = FindViewById<ViewPager>(Resource.Id.pager);
+            var tabLayout = FindViewById<TabLayout>(Resource.Id.sliding_tabs);
+            var adapter = new CustomPagerAdapter(this, SupportFragmentManager);
+            var toolbar = FindViewById<Toolbar>(Resource.Id.my_toolbar);
+
+            // Setup Toolbar
+            SetSupportActionBar(toolbar);
+            SupportActionBar.Title = "HelloSwipeViewWithTabs";
+
+            // Set adapter to view pager
+            pager.Adapter = adapter;
+
+            // Setup tablayout with view pager
+            tabLayout.SetupWithViewPager(pager);
+
+            // Iterate over all tabs and set the custom view
+            for (int i = 0; i < tabLayout.TabCount; i++)
+            {
+                TabLayout.Tab tab = tabLayout.GetTabAt(i);
+                tab.SetCustomView(adapter.GetTabView(i));
+            }
+        }
+    }
+}
+
