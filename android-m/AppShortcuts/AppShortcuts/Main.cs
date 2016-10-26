90d13e053fcb20c9b4341c59a1618b3a9ab71e32
Gonzalo Martin <gonzalo.martin@globallogic.com>
2016-10-26 11:22:52 -0300

    Ported code

--- android-m/AppShortcuts/AppShortcuts/Main.cs
+++ android-m/AppShortcuts/AppShortcuts/Main.cs
@@ -14,23 +14,31 @@
  * limitations under the License.
  */
 
+using System.Collections.Generic;
 using Android.App;
+using Android.Content;
 using Android.Widget;
 using Android.OS;
+using Android.Text;
+using Android.Util;
+using Android.Views;
+using Android.Views.InputMethods;
+using Java.Lang;
 
 namespace AppShortcuts
 {
 	[Activity(Label = "AppShortcuts", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class Main : ListActivity
+	[MetaData("android.app.shortcuts", Resource = "@xml/shortcuts")]
+	public class Main : ListActivity, View.IOnClickListener
 	{
 		public static string TAG = "ShortcutSample";
 
-    	static string ID_ADD_WEBSITE = "add_website";
+		static string ID_ADD_WEBSITE = "add_website";
 
-    	static string ACTION_ADD_WEBSITE = "com.example.android.shortcutsample.ADD_WEBSITE";
-		//private MyAdapter myAdapter;
+		static string ACTION_ADD_WEBSITE = "com.example.android.shortcutsample.ADD_WEBSITE";
+		private MyAdapter mAdapter;
 
-		private ShortcutHelper shortcutHelper;
+		private ShortcutHelper mHelper;
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -39,11 +47,231 @@ namespace AppShortcuts
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+			mHelper = new ShortcutHelper(this);
 
-			//button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+			mHelper.MaybeRestoreAllDynamicShortcuts();
+
+			mHelper.RefreshShortcuts(/*force=*/ false);
+
+			if (ACTION_ADD_WEBSITE.Equals(Intent.Action))
+			{
+				// Invoked via the manifest shortcut.
+				AddWebSite();
+			}
+
+			mAdapter = new MyAdapter(this);
+			ListAdapter = mAdapter;
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+			RefreshList();
+		}
+
+		/**
+		 * Handle the add button.
+		 */
+		public void OnAddPressed(View v)
+		{
+			AddWebSite();
+		}
+
+		private void AddWebSite()
+		{
+			Log.Info(TAG, "addWebSite");
+
+			// This is important.  This allows the launcher to build a prediction model.
+			mHelper.ReportShortcutUsed(ID_ADD_WEBSITE);
+
+			EditText editUri = new EditText(this);
+
+			editUri.SetHint("http://www.xamarin.com/");
+			editUri.InputType = InputTypes.TextVariationUri;
+
+			new AlertDialog.Builder(this)
+				.SetTitle("Add new website")
+				.SetMessage("Type URL of a website")
+				.SetView(editUri)
+				.SetPositiveButton("Add", (dialog, whichButton) =>
+				{
+					string url = editUri.Text.ToString().Trim();
+					if (url.Length > 0)
+					{
+						AddUriAsync(url);
+					}
+				})
+				.Show();
+		}
+
+		private void AddUriAsync(string url)
+		{
+			new AddUriTask(this, mHelper);
+		}
+
+		private class AddUriTask : AsyncTask<Java.Lang.Void, Java.Lang.Void, Java.Lang.Void>
+		{
+			private ShortcutHelper Helper { get; set; }
+			private Main Owner { get; set; }
+
+			public AddUriTask(Main owner, ShortcutHelper helper)
+			{
+				Owner = owner;
+				Helper = helper;
+			}
+
+			protected override Void DoInBackground(params Void[] @params)
+			{
+				var uri = (string) @params[0];
+				Helper.AddWebSiteShortcut(uri);
+				return null;
+			}
+
+			protected override void OnPostExecute(Void result)
+			{
+				Owner.RefreshList();
+			}
+		}
+
+		private void RefreshList()
+		{
+			mAdapter.SetShortcuts(mHelper.GetShortcuts());
+		}
+
+		public void OnClick(View view)
+		{
+			ShortcutInfo shortcut = (ShortcutInfo) ((View) view.Parent).Tag;
+
+			switch (view.Id)
+			{
+				case Resource.Id.disable:
+					if (shortcut.IsEnabled())
+					{
+						mHelper.DisableShortcut(shortcut);
+					}
+					else
+					{
+						mHelper.EnableShortcut(shortcut);
+					}
+					RefreshList();
+					break;
+				case Resource.Id.remove:
+					mHelper.RemoveShortcut(shortcut);
+					RefreshList();
+					break;
+			}
+		}
+
+		private static List<ShortcutInfo> EMPTY_LIST = new List<ShortcutInfo>();
+
+		private string GetType(ShortcutInfo shortcut)
+		{
+			StringBuilder sb = new StringBuilder();
+			string sep = "";
+			if (shortcut.IsDynamic())
+			{
+				sb.Append(sep);
+				sb.Append("Dynamic");
+				sep = ", ";
+			}
+			if (shortcut.IsPinned())
+			{
+				sb.Append(sep);
+				sb.Append("Pinned");
+				sep = ", ";
+			}
+			if (!shortcut.IsEnabled())
+			{
+				sb.Append(sep);
+				sb.Append("Disabled");
+				sep = ", ";
+			}
+			return sb.toString();
+		}
+
+		private class MyAdapter : BaseAdapter
+		{
+			private Context Context { get; set; }
+			private LayoutInflater Inflater;
+			private List<ShortcutInfo> mList = EMPTY_LIST;
+			private Main Owner { get; set; }
+
+			public MyAdapter(Context context)
+			{
+				Context = context;
+				Inflater = Context.GetSystemService(typeof (LayoutInflater));
+				Owner = (Main) context;
+			}
+
+			public override int Count
+			{
+				get
+				{
+					return mList.Count;
+				}
+			}
+
+			public override Object GetItem(int position)
+			{
+				return (Object) mList[position];
+			}
+
+			public override long GetItemId(int position)
+			{
+				return position;
+			}
+
+			public override bool HasStableIds { get { return false; } }
+
+			public override bool IsEnabled(int position)
+			{
+				return true;
+			}
+
+			public void SetShortcuts(List<ShortcutInfo> list)
+			{
+				mList = list;
+				NotifyDataSetChanged();
+			}
+
+			public override View GetView(int position, View convertView, ViewGroup parent)
+			{
+				View view;
+				if (convertView != null)
+				{
+					view = convertView;
+				}
+				else
+				{
+					view = Inflater.Inflate(Resource.Layout.ListItem, null);
+				}
+
+				BindView(view, position, mList[position]);
+
+				return view;
+			}
+
+			public void BindView(View view, int position, ShortcutInfo shortcut)
+			{
+				view.Tag = shortcut;
+
+				TextView line1 = view.FindViewById<TextView>(Resource.Id.line1);
+				TextView line2 = view.FindViewById<TextView>(Resource.Id.line2);
+
+				line1.Text = shortcut.GetLongLabel();
+
+				line2.Text = Owner.GetType(shortcut);
+
+				Button remove = view.FindViewById<Button>(Resource.Id.remove);
+				Button disable = view.FindViewById<Button>(Resource.Id.disable);
+
+				disable.Text = shortcut.IsEnabled()
+					? Owner.GetString(Resource.String.disable_shortcut)
+					: Owner.GetString(Resource.String.enable_shortcut);
+
+				remove.SetOnClickListener(Owner);
+				disable.SetOnClickListener(Owner);
+			}
 		}
 	}
 }
--- android-m/AppShortcuts/AppShortcuts/ShortcutHelper.cs
+++ android-m/AppShortcuts/AppShortcuts/ShortcutHelper.cs
@@ -16,18 +16,27 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Util;
+using Void = Java.Lang.Void;
+using Java.Net;
+using Java.Util;
+using IOException = Java.IO.IOException;
+using Uri = Android.Net.Uri;
 
 namespace AppShortcuts
 {
 	public class ShortcutHelper
 	{
-
 		private static string TAG = Main.TAG;
 
-    	private static string EXTRA_LAST_REFRESH = "com.example.android.shortcutsample.EXTRA_LAST_REFRESH";
+		private static string EXTRA_LAST_REFRESH = "com.example.android.shortcutsample.EXTRA_LAST_REFRESH";
 
-    	private static long REFRESH_INTERVAL_MS = 60 * 60 * 1000;
+		private static long REFRESH_INTERVAL_MS = 60 * 60 * 1000;
 
 		private Context mContext;
 
@@ -36,12 +45,12 @@ namespace AppShortcuts
 		public ShortcutHelper(Context context)
 		{
 			mContext = context;
-			mShortcutManager = mContext.GetSystemService(ShortcutManager.class));
-    	}
-	
+			//mShortcutManager = mContext.getSystemService(ShortcutManager.class);
+		}
+
 		public void MaybeRestoreAllDynamicShortcuts()
 		{
-			if (mShortcutManager.getDynamicShortcuts().size() == 0)
+			if (mShortcutManager.GetDynamicShortcuts().size() == 0)
 			{
 				// NOTE: If this application is always supposed to have dynamic shortcuts, then publish
 				// them here.
@@ -56,53 +65,208 @@ namespace AppShortcuts
 		}
 
 		/**
-     	 * Use this when interacting with ShortcutManager to show consistent error messages.
-	     */
-	    private void CallShortcutManager(BooleanSupplier r)
+		 * Use this when interacting with ShortcutManager to show consistent error messages.
+		 */
+		private void CallShortcutManager(BooleanSupplier r)
 		{
 			try
 			{
-				if (!r.getAsBoolean())
+				if (!r.GetAsBoolean())
 				{
 					Utils.ShowToast(mContext, "Call to ShortcutManager is rate-limited");
 				}
 			}
 			catch (Exception e)
 			{
-				Log.e(TAG, "Caught Exception", e);
-				Utils.ShowToast(mContext, "Error while calling ShortcutManager: " + e.toString());
+				Log.Error(TAG, "Caught Exception", e);
+				Utils.ShowToast(mContext, "Error while calling ShortcutManager: " + e.ToString());
 			}
 		}
 
 		/**
-	     * Return all mutable shortcuts from this app self.
-	     */
-	    public List<ShortcutInfo> GetShortcuts()
+		 * Return all mutable shortcuts from this app self.
+		 */
+		public List<ShortcutInfo> GetShortcuts()
 		{
 			// Load mutable dynamic shortcuts and pinned shortcuts and put them into a single list
 			// removing duplicates.
 
-			final List< ShortcutInfo> ret = new ArrayList<ShortcutInfo>();
-			final HashSet< String > seenKeys = new HashSet<>();
+			List<ShortcutInfo> ret = new List<ShortcutInfo>();
+			HashSet<string> seenKeys = new HashSet<string>();
 
 			// Check existing shortcuts shortcuts
-			for (ShortcutInfo shortcut : mShortcutManager.GetDynamicShortcuts())
+			foreach (var shortcut in mShortcutManager.getDynamicShortcuts())
 			{
-				if (!shortcut.isImmutable())
-				{
-					ret.add(shortcut);
-					seenKeys.add(shortcut.getId());
-				}
+				if (shortcut.IsImmutable()) continue;
+				ret.Add(shortcut);
+				seenKeys.Add(shortcut.GetId());
 			}
-			for (ShortcutInfo shortcut : mShortcutManager.getPinnedShortcuts())
+
+			foreach (var shortcut in mShortcutManager.getPinnedShortcuts())
 			{
-				if (!shortcut.isImmutable() && !seenKeys.contains(shortcut.getId()))
-				{
-					ret.add(shortcut);
-					seenKeys.add(shortcut.getId());
-				}
+				if (shortcut.IsImmutable() || seenKeys.Contains(shortcut.GetId())) continue;
+				ret.Add(shortcut);
+				seenKeys.Add(shortcut.GetId());
 			}
+			
 			return ret;
+		}
+
+		public void RefreshShortcuts(bool force)
+		{
+			new RefreshShortcutsTask(mContext, this).Execute(force);
+		}
+
+		private class RefreshShortcutsTask : AsyncTask<Java.Lang.Void, Java.Lang.Void, Java.Lang.Void>
+		{
+			private ShortcutHelper Helper { get; set; }
+			private Context Context { get; set; }
+
+			public RefreshShortcutsTask(Context context, ShortcutHelper helper)
+			{
+				Context = context;
+				Helper = helper;
+			}
+			protected override Void RunInBackground(params Void[] @params)
+			{
+				Log.Info(TAG, "refreshingShortcuts...");
+
+				var force = (bool) @params[0];
+
+				var now = Java.Lang.JavaSystem.CurrentTimeMillis();
+				var staleThreshold = force ? now : now - REFRESH_INTERVAL_MS;
+
+				// Check all existing dynamic and pinned shortcut, and if their last refresh
+				// time is older than a certain threshold, update them.
+
+				List<ShortcutInfo> updateList = new List<ShortcutInfo>();
+
+				foreach (var shortcut in updateList)
+				{
+					if (shortcut.IsImmutable())
+					{
+						continue;
+					}
+
+					PersistableBundle extras = shortcut.GetExtras();
+
+					if (extras != null && extras.GetLong(EXTRA_LAST_REFRESH) >= staleThreshold)
+					{
+						// Shortcut still fresh.
+						continue;
+					}
+
+					Log.Info(TAG, "Refreshing shortcut: " + shortcut.GetId());
+
+					ShortcutInfo.Builder b = new ShortcutInfo.Builder(Context, shortcut.GetId());
+
+					Helper.SetSiteInformation(b, shortcut.GetIntent().getData());
+
+					Helper.SetExtras(b);
+
+					updateList.Add(b.Build());
+				}
+
+				// Call update.
+				if (updateList.Count > 0)
+				{
+					Helper.CallShortcutManager(()->mShortcutManager.updateShortcuts(updateList));
+				}
+
+				return null;
+			}
+		}
+
+		private ShortcutInfo CreateShortcutForUrl(string urlAsString)
+		{
+			Log.Info(TAG, "createShortcutForUrl: " + urlAsString);
+
+			ShortcutInfo.Builder b = new ShortcutInfo.Builder(mContext, urlAsString);
+
+			var uri = Uri.Parse(urlAsString);
+			b.SetIntent(new Intent(Intent.ActionView, uri));
+
+			SetSiteInformation(b, uri);
+			SetExtras(b);
+
+			return b.Build();
+		}
+
+		private ShortcutInfo.Builder SetSiteInformation(ShortcutInfo.Builder b, Uri uri)
+		{
+			// TODO Get the actual site <title> and use it.
+			// TODO Set the current locale to accept-language to get localized title.
+			b.SetShortLabel(uri.Host);
+			b.SetLongLabel(uri.ToString());
+
+			var bmp = FetchFavicon(uri);
+			b.SetIcon(bmp != null ? Icon.CreateWithBitmap(bmp) : Icon.CreateWithResource(mContext, Resource.Drawable.link));
+
+			return b;
+		}
+
+		private ShortcutInfo.Builder SetExtras(ShortcutInfo.Builder b)
+		{
+			var extras = new PersistableBundle();
+			extras.PutLong(EXTRA_LAST_REFRESH, Java.Lang.JavaSystem.CurrentTimeMillis());
+			b.SetExtras(extras);
+			return b;
+		}
+
+		private string NormalizeUrl(string urlAsString)
+		{
+			if (urlAsString.StartsWith("http://") || urlAsString.StartsWith("https://"))
+			{
+				return urlAsString;
+			}
+			return "http://" + urlAsString;
+		}
+
+		public void AddWebSiteShortcut(string urlAsString)
+		{
+			var uriFinal = urlAsString;
+			CallShortcutManager(()-> {
+				ShortcutInfo shortcut = CreateShortcutForUrl(NormalizeUrl(uriFinal));
+				return mShortcutManager.AddDynamicShortcuts(Arrays.AsList(shortcut));
+			});
+		}
+
+		public void RemoveShortcut(ShortcutInfo shortcut)
+		{
+			mShortcutManager.RemoveDynamicShortcuts(Arrays.AsList(shortcut.GetId()));
+		}
+
+		public void DisableShortcut(ShortcutInfo shortcut)
+		{
+			mShortcutManager.DisableShortcuts(Arrays.AsList(shortcut.GetId()));
+		}
+
+		public void EnableShortcut(ShortcutInfo shortcut)
+		{
+			mShortcutManager.EnableShortcuts(Arrays.AsList(shortcut.GetId()));
+		}
+
+		private Bitmap FetchFavicon(Uri uri)
+		{
+			var iconUri = uri.BuildUpon().Path("favicon.ico").Build();
+			Log.Info(TAG, "Fetching favicon from: " + iconUri);
+
+			BufferedStream bis = null;
+			Stream inputStream;
+			try
+			{
+				var conn = new URL(iconUri.ToString()).OpenConnection();
+				conn.Connect();
+
+				inputStream = conn.InputStream;
+				bis = new BufferedStream(inputStream, 8192);
+				return BitmapFactory.DecodeStream(bis);
+			}
+			catch (IOException e)
+			{
+				Log.Warn(TAG, "Failed to fetch favicon from " + iconUri, e);
+				return null;
+			}
 		}
 	}
 }
