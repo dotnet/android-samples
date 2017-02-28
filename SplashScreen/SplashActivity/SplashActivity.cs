94cb1c60fda98605be9c429be5acd8632e6ba0dd
Mark McLemore <mgmclemore@gmail.com>
2017-02-27 16:59:50 -0800

    Update SplashScreen to use async/await, hide titlebar (doc #1877)

--- SplashScreen/SplashActivity/SplashActivity.cs
+++ SplashScreen/SplashActivity/SplashActivity.cs
@@ -18,24 +18,24 @@ namespace com.xamarin.sample.splashscreen
             Log.Debug(TAG, "SplashActivity.OnCreate");
         }
 
+        // Launches the startup task
         protected override void OnResume()
         {
             base.OnResume();
-
-            Task startupWork = new Task(() =>
-                                        {
-                                            Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
-                                            Task.Delay(5000); // Simulate a bit of startup work.
-                                            Log.Debug(TAG, "Working in the background - important stuff.");
-                                        });
-
-            startupWork.ContinueWith(t =>
-                                     {
-                                         Log.Debug(TAG, "Work is finished - start Activity1.");
-                                         StartActivity(new Intent(Application.Context, typeof (Activity1)));
-                                     }, TaskScheduler.FromCurrentSynchronizationContext());
-
+            Task startupWork = new Task(() => { SimulateStartup(); });
             startupWork.Start();
+        }
+
+        // Prevent the back button from canceling the startup process
+        public override void OnBackPressed() { }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup ()
+        {
+            Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
+            await Task.Delay(8000); // Simulate a bit of startup work.
+            Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
+            StartActivity(new Intent(Application.Context, typeof (MainActivity)));
         }
     }
 }
