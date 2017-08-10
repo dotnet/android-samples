d2bac57ab5af5da85d70ca102d5c0b051ef6d709
Gonzalo Martin <gonzalo.martin@globallogic.com>
2017-08-10 17:29:48 -0300

    Initial code

---
+++ android-o/EmojiCompat/EmojiCompat/CustomTextView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.Content;
+using Android.Support.V7.Widget;
+
+namespace EmojiCompat
+{
+	/**
+ 	 * A sample implementation of custom TextView.
+ 	 *
+ 	 * <p>You can use {@link EmojiTextViewHelper} to make your custom TextView compatible with
+ 	 * EmojiCompat.</p>
+ 	 */
+	public class CustomTextView : AppCompatTextView
+	{
+		EmojiTextViewHelper mEmojiTextViewHelper;
+
+		public CustomTextView(Context context) : base(context, null)
+		{
+		}
+	}
+}
---
+++ android-o/EmojiCompat/EmojiCompat/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace EmojiCompat
+{
+	[Activity(Label = "EmojiCompat", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.activity_main);
+		}
+	}
+}
+
