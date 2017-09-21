70ed75881b86fe8c54a8939da8536d6b21154b3f
Morten Nielsen <mn@iter.dk>
2017-09-21 07:22:04 -0700

    Add IntentFilter for Android TV Leanback app (#212)
    * Add IntentFilter for Android TV
    
    Without this intent, the app tile will not be added to Android TV Apps. Full documentation here: https://developer.android.com/training/tv/start/start.html#dev-project
    
    * Fixed indent

--- tv/TvLeanback/TvLeanback/MainActivity.cs
+++ tv/TvLeanback/TvLeanback/MainActivity.cs
@@ -6,6 +6,9 @@ namespace TvLeanback
 {
 	[Activity (Icon="@drawable/videos_by_google_banner", Label="@string/app_name", 
 		ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape, MainLauncher = true)]
+	[IntentFilter(
+		actions : new string[] { "android.intent.action.MAIN" },
+		Categories = new string[] { "android.intent.category.LEANBACK_LAUNCHER" })]
 	public class MainActivity : Activity
 	{
 		protected override void OnCreate(Bundle saved)
