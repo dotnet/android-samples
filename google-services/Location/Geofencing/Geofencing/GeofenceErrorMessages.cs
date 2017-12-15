6ae4ddd79e0f00131b44fac9bb96aa0cd721a89f
Dmytro <malkin.dmytro@gmail.com>
2017-12-15 16:23:17 +0200

    Geofencing updated (#247)
    * Geofencing updated
    
    * Review comments resolved
    * Spacing reverted in Constants.cs
    * Xamarin.Android.Support packages changed to v26.1.0.1
    * IOnCompleteListener cast removed
    * OnRequestPermissionsResult signature changed

--- google-services/Location/Geofencing/Geofencing/GeofenceErrorMessages.cs
+++ google-services/Location/Geofencing/Geofencing/GeofenceErrorMessages.cs
@@ -1,25 +1,38 @@
 ﻿using System;
 using Android.Content;
+using Android.Gms.Common.Apis;
 using Android.Gms.Location;
 
 namespace Geofencing
 {
-	public static class GeofenceErrorMessages
-	{
-		public static string GetErrorString (Context context, int errorCode)
-		{
-			var mResources = context.Resources;
-			switch (errorCode) {
-			case GeofenceStatusCodes.GeofenceNotAvailable:
-				return mResources.GetString (Resource.String.geofence_not_available);
-			case GeofenceStatusCodes.GeofenceTooManyGeofences:
-				return mResources.GetString (Resource.String.geofence_too_many_geofences);
-			case GeofenceStatusCodes.GeofenceTooManyPendingIntents:
-				return mResources.GetString (Resource.String.geofence_too_many_pending_intents);
-			default:
-				return mResources.GetString (Resource.String.unknown_geofence_error);
-			}
-		}
-	}
+    public static class GeofenceErrorMessages
+    {
+        public static String GetErrorString(Context context, Exception ex)
+        {
+            if (ex is ApiException)
+            {
+                return GetErrorString(context, ((ApiException)ex).StatusCode);
+            }
+            else
+            {
+                return context.Resources.GetString(Resource.String.unknown_geofence_error);
+            }
+        }
+        public static string GetErrorString(Context context, int errorCode)
+        {
+            var mResources = context.Resources;
+            switch (errorCode)
+            {
+                case GeofenceStatusCodes.GeofenceNotAvailable:
+                    return mResources.GetString(Resource.String.geofence_not_available);
+                case GeofenceStatusCodes.GeofenceTooManyGeofences:
+                    return mResources.GetString(Resource.String.geofence_too_many_geofences);
+                case GeofenceStatusCodes.GeofenceTooManyPendingIntents:
+                    return mResources.GetString(Resource.String.geofence_too_many_pending_intents);
+                default:
+                    return mResources.GetString(Resource.String.unknown_geofence_error);
+            }
+        }
+    }
 }
 
