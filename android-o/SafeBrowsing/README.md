c46741c13d19d3e1d7db5d739da03453d2a7ccd6
Dmytro Malkin <v-dmamalk@microsoft.com>
2018-01-09 15:41:00 +0200

    Safe browsing sample created

---
+++ android-o/SafeBrowsing/README.md
@@ -0,0 +1,12 @@
+SafeBrowsing
+=====
+
+SafeBrowsing is a simple project which demostrates safe brosing on android web view. This is new feature of android 8.1 (API 27).
+
+In order to reach desired behavior Android manifest should has android.webkit.WebView.EnableSafeBrowsing flag set to true in metadata section.
+StartSafeBrowsing method were added and gives possibility to react on malware links.
+
+Authors
+-------
+
+Malkin Dmytro
---
+++ android-o/SafeBrowsing/SafeBrowsing/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Webkit;
+using System;
+
+namespace SafeBrowsing
+{
+    [Activity(Label = "SafeBrowsing", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        public WebView webView;
+        public bool mSafeBrowsingIsInitialized;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            webView = FindViewById<WebView>(Resource.Id.webview);
+            webView.SetWebViewClient(new WebViewClientImpl { Activity = this });
+
+            mSafeBrowsingIsInitialized = false;
+            var valueCallback = new ValueCallback { MainActivity = this };
+            WebView.StartSafeBrowsing(this, valueCallback);
+            webView.LoadUrl("https://university.xamarin.com/");
+        }
+    }
+
+    public class ValueCallback : Java.Lang.Object, IValueCallback
+    {
+        public MainActivity MainActivity;
+        public void OnReceiveValue(Java.Lang.Object value)
+        {
+            MainActivity.mSafeBrowsingIsInitialized = true;
+            MainActivity.ActionBar.SetTitle(Convert.ToBoolean(value) ? Resource.String.safe_status : Resource.String.unsafe_status);
+        }
+    }
+}
+
