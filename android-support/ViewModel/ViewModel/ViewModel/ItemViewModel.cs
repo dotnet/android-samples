cada55498c0f49cfa05071864644948d0c5195cf
Gonzalo Martin <lalosoft@gmail.com>
2018-01-25 16:35:26 -0300

    [Support Components] ViewModel sample (#258)
    * Created sample
    
    * Moved sample into android-support folder
    
    * Removed .vs folder
    
    * Re-imported nuget packages from VS
    
    * Updated target of xamarin.android.support packages to MonoAndroid81

---
+++ android-support/ViewModel/ViewModel/ViewModel/ItemViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ViewModel
+{
+	public class ItemViewModel : Android.Arch.Lifecycle.ViewModel
+	{
+		List<Item> Items;
+
+		public List<Item> GetItems()
+		{
+			if (Items == null)
+				Items = LoadItems();
+			return Items;
+		}
+
+		List<Item> LoadItems()
+		{
+			Items = new List<Item>();
+			for (var i = 0; i < 10; i++)
+			{
+				Items.Add(new Item { Name = "Item " + i});
+			}
+			return Items;
+		}
+	}
+}
