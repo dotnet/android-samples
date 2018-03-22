8917e3961b9f4fa466e0fa48c0f3791a50b4addd
Mark McLemore <mgmclemore@gmail.com>
2018-03-22 15:46:36 -0700

    Update ContactsProviderDemo to use CursorLoader

--- ContactsProviderDemo/MainActivity.cs
+++ ContactsProviderDemo/MainActivity.cs
@@ -1,5 +1,4 @@
 using System;
-
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -7,11 +6,12 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Provider;
+using Android.Database;
 
 namespace ContactsProviderDemo
 {
     [Activity (Label = "ContactsProviderDemo", MainLauncher = true)]
-    public class Activity1 : Activity
+    public class MainActivity : Activity
     {
         protected override void OnCreate (Bundle bundle)
         {
@@ -32,45 +32,58 @@ namespace ContactsProviderDemo
         
         void GetContacts ()
         {
+            // Get the URI for the user's contacts:
             var uri = ContactsContract.Contacts.ContentUri;
             
+            // Setup the "projection" (columns we want) for only the ID and display name:
             string[] projection = { 
                 ContactsContract.Contacts.InterfaceConsts.Id, ContactsContract.Contacts.InterfaceConsts.DisplayName };
-            
-            var cursor = ManagedQuery (uri, projection, null, null, null);
-            
-            if (cursor.MoveToFirst ()) {
-                do {
-                    Console.WriteLine ("Contact ID: {0}, Contact Name: {1}", 
-                                       cursor.GetString (cursor.GetColumnIndex (projection [0])),
-                                       cursor.GetString (cursor.GetColumnIndex (projection [1])));
-                    
-                } while (cursor.MoveToNext());
+
+            // Use a CursorLoader to retrieve the user's contacts data:
+            CursorLoader loader = new CursorLoader(this, uri, projection, null, null, null);
+            ICursor cursor = (ICursor)loader.LoadInBackground();
+
+            // Print the contact data (ID and DisplayName) to the console if reading back succeeds:
+            if (cursor != null)
+            {
+                if (cursor.MoveToFirst())
+                {
+                    do
+                    {
+                        Console.WriteLine("Contact ID: {0}, Contact Name: {1}",
+                                           cursor.GetString(cursor.GetColumnIndex(projection[0])),
+                                           cursor.GetString(cursor.GetColumnIndex(projection[1])));
+                    } while (cursor.MoveToNext());
+                }
             }
         }
         
         void UpdateProfile ()
         {   
-            // write to the profile
+            // Write to the profile
             var values = new ContentValues ();
-            
             values.Put (ContactsContract.Contacts.InterfaceConsts.DisplayName, "John Doe");
-            
             ContentResolver.Update (ContactsContract.Profile.ContentRawContactsUri, values, null, null);
             
-            // read the profile
+            // Read the profile
             var uri = ContactsContract.Profile.ContentUri;
             
+            // Setup the "projection" (column we want) for only the display name:
             string[] projection = { 
                 ContactsContract.Contacts.InterfaceConsts.DisplayName };
             
-            var cursor = ManagedQuery (uri, projection, null, null, null);
-
-            if (cursor.MoveToFirst ()) {
-                Console.WriteLine(cursor.GetString (cursor.GetColumnIndex (projection [0])));
+            // Use a CursorLoader to retrieve the data:
+            CursorLoader loader = new CursorLoader(this, uri, projection, null, null, null);
+            ICursor cursor = (ICursor)loader.LoadInBackground();
+            if (cursor != null)
+            { 
+                if (cursor.MoveToFirst ())
+                {
+                    Console.WriteLine(cursor.GetString (cursor.GetColumnIndex (projection [0])));
+                }
             }
             
-            // navigate to the profile in the people app
+            // Navigate to the profile in the contacts app
             var intent = new Intent (Intent.ActionView, ContactsContract.Profile.ContentUri);            
             StartActivity (intent);    
         }
--- ContactsProviderDemo/README.md
+++ ContactsProviderDemo/README.md
@@ -1,4 +1,5 @@
 ContactsProvider Demo
 =====================
 
-This example shows how to use the ContactsProvider as well as the device owner's profile in Ice Cream Sandwich.
+This example shows how to use the `ContactsProvider` as well as the
+device owner's profile in Android.
