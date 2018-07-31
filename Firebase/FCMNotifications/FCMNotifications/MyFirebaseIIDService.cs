caa8bb6cd18176c5fbbcd15a8a8393441d070519
Tom Opgenorth <tom@opgenorth.net>
2018-07-31 11:54:35 -0600

    Update FCM notifications example.
    See:
    
    * https://github.com/MicrosoftDocs/xamarin-docs-pr/issues/211
    * https://github.com/MicrosoftDocs/xamarin-docs-pr/pull/512

--- Firebase/FCMNotifications/FCMNotifications/MyFirebaseIIDService.cs
+++ Firebase/FCMNotifications/FCMNotifications/MyFirebaseIIDService.cs
@@ -1,21 +1,25 @@
 ﻿using System;
+
 using Android.App;
-using Firebase.Iid;
 using Android.Util;
+
+using Firebase.Iid;
 
 namespace FCMNotifications
 {
     [Service]
-    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    [IntentFilter(new[] {"com.google.firebase.INSTANCE_ID_EVENT"})]
     public class MyFirebaseIIDService : FirebaseInstanceIdService
     {
         const string TAG = "MyFirebaseIIDService";
+
         public override void OnTokenRefresh()
         {
             var refreshedToken = FirebaseInstanceId.Instance.Token;
             Log.Debug(TAG, "Refreshed token: " + refreshedToken);
             SendRegistrationToServer(refreshedToken);
         }
+
         void SendRegistrationToServer(string token)
         {
             // Add custom implementation, as needed.
--- Firebase/FCMNotifications/FCMNotifications/MyFirebaseMessagingService.cs
+++ Firebase/FCMNotifications/FCMNotifications/MyFirebaseMessagingService.cs
@@ -1,45 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+
 using Android.App;
 using Android.Content;
-using Android.Media;
+using Android.OS;
+using Android.Support.V4.App;
 using Android.Util;
+
 using Firebase.Messaging;
-using FCMNotifications;
-using System.Collections.Generic;
 
 namespace FCMNotifications
 {
     [Service]
-    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    [IntentFilter(new[] {"com.google.firebase.MESSAGING_EVENT"})]
     public class MyFirebaseMessagingService : FirebaseMessagingService
     {
         const string TAG = "MyFirebaseMsgService";
+
         public override void OnMessageReceived(RemoteMessage message)
         {
             Log.Debug(TAG, "From: " + message.From);
-            Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
-            SendNotification(message.GetNotification().Body, message.Data);
+
+            var body = message.GetNotification().Body;
+            Log.Debug(TAG, "Notification Message Body: " + body);
+            SendNotification(body, message.Data);
         }
 
         void SendNotification(string messageBody, IDictionary<string, string> data)
         {
             var intent = new Intent(this, typeof(MainActivity));
             intent.AddFlags(ActivityFlags.ClearTop);
-            foreach (string key in data.Keys)
+            foreach (var key in data.Keys)
             {
                 intent.PutExtra(key, data[key]);
             }
-            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
 
-            var notificationBuilder = new Notification.Builder(this)
-                .SetSmallIcon(Resource.Drawable.ic_stat_ic_notification)
-                .SetContentTitle("FCM Message")
-                .SetContentText(messageBody)
-                .SetAutoCancel(true)
-                .SetContentIntent(pendingIntent);
+            var pendingIntent = PendingIntent.GetActivity(this, MainActivity.NOTIFICATION_ID, intent, PendingIntentFlags.OneShot);
 
-            var notificationManager = NotificationManager.FromContext(this);
-            notificationManager.Notify(0, notificationBuilder.Build());
+            var notificationBuilder = new NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
+                                      .SetSmallIcon(Resource.Drawable.ic_stat_ic_notification)
+                                      .SetContentTitle("FCM Message")
+                                      .SetContentText(messageBody)
+                                      .SetAutoCancel(true)
+                                      .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManagerCompat.From(this);
+            notificationManager.Notify(MainActivity.NOTIFICATION_ID, notificationBuilder.Build());
         }
+
+
     }
 }
