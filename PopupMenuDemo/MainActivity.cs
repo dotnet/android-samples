cae0aff4722d6ef0f15db0d6d6d504c2303b62d2
Mark McLemore <mamcle@microsoft.com>
2018-07-31 14:30:11 -0700

    Update PopupMenuDemo and add new screenshot

--- PopupMenuDemo/MainActivity.cs
+++ PopupMenuDemo/MainActivity.cs
@@ -1,5 +1,4 @@
 using System;
-
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -24,17 +23,15 @@ namespace PopupMenuDemo
                 
                 PopupMenu menu = new PopupMenu (this, showPopupMenu);
                 
-                // with Android 3 need to use MenuInfater to inflate the menu
-                //menu.MenuInflater.Inflate (Resource.Menu.popup_menu, menu.Menu);
-                
-                // with Android 4 Inflate can be called directly on the menu
+                // Call inflate directly on the menu:
                 menu.Inflate (Resource.Menu.popup_menu);
                 
+                // A menu item was clicked:
                 menu.MenuItemClick += (s1, arg1) => {
                     Console.WriteLine ("{0} selected", arg1.Item.TitleFormatted);
                 };
                 
-                // Android 4 now has the DismissEvent
+                // Menu was dismissed:
                 menu.DismissEvent += (s2, arg2) => {
                     Console.WriteLine ("menu dismissed"); 
                 };
--- PopupMenuDemo/README.md
+++ PopupMenuDemo/README.md
@@ -1,4 +1,7 @@
 Popup Menu Demo
 ===============
 
-This example shows how to use a Popup Menu with Ice Cream Sandwich.
+**PopupMenuDemo** is a sample app that accompanies the article,
+[PopUp Menu](https://docs.microsoft.com/en-us/xamarin/android/user-interface/controls/popup-menu).
+It demonstrates how to add support for displaying popup menus that are attached to
+a particular view.
