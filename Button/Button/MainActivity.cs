4a2e0bed77afb8018cc99c5af17bfcde4788ea4f
Giovanny Hernandez <87216285+officialgio@users.noreply.github.com>
2022-07-07 10:21:13 -0700

    New Button Sample port

---
+++ Button/Button/MainActivity.cs
@@ -0,0 +1,20 @@
+namespace Mono.Samples.Button;
+
+[Activity(Label = "Button Demo", MainLauncher = true)]
+public class ButtonActivity : Activity
+{
+    int count = 0;
+
+    protected override void OnCreate(Bundle? bundle)
+    {
+        base.OnCreate(bundle);
+
+        // Create your application here
+        Android.Widget.Button button = new(this);
+
+        button.Text = string.Format("{0} clicks!!", count);
+        button.Click += delegate { button.Text = string.Format("{0} clicks!!", ++count); };
+
+        SetContentView(button);
+    }
+}
