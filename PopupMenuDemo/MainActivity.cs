128e99fa66d175364810afaee53f185adee8511d
Leandra Hall <90208577+LeandraHall@users.noreply.github.com>
2022-07-11 08:51:31 -0700

    Update MainActivity.cs

--- PopupMenuDemo/MainActivity.cs
+++ PopupMenuDemo/MainActivity.cs
@@ -1,46 +1,37 @@
-using System;
-using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
+namespace PopupMenuDemo;
 
-namespace PopupMenuDemo
+[Activity(Label = "PopupMenuDemo", MainLauncher = true)]
+public class Activity1 : Activity
 {
-    [Activity(Label = "PopupMenuDemo", MainLauncher = true)]
-    public class Activity1 : Activity
+    protected override void OnCreate(Bundle? bundle)
     {
-        protected override void OnCreate(Bundle? bundle)
-        {
-            base.OnCreate(bundle);
+        base.OnCreate(bundle);
 
-            SetContentView(Resource.Layout.Main);
+        SetContentView(Resource.Layout.Main);
 
-            Button showPopupMenu = FindViewById<Button>(Resource.Id.popupButton)!;
+        Button showPopupMenu = FindViewById<Button>(Resource.Id.popupButton)!;
 
-            if (showPopupMenu is null)
-                throw new NullReferenceException();
+        if (showPopupMenu is null)
+            throw new NullReferenceException();
 
-            showPopupMenu.Click += (s, arg) => {
+        showPopupMenu.Click += (s, arg) => {
 
-                PopupMenu menu = new PopupMenu(this, showPopupMenu);
+            PopupMenu menu = new PopupMenu(this, showPopupMenu);
 
-                // Call inflate directly on the menu:
-                menu.Inflate(Resource.Menu.popup_menu);
+            // Call inflate directly on the menu:
+            menu.Inflate(Resource.Menu.popup_menu);
 
-                // A menu item was clicked:
-                menu.MenuItemClick += (s1, arg1) => {
-                    Console.WriteLine("{0} selected", arg1.Item!.TitleFormatted);
-                };
-
-                // Menu was dismissed:
-                menu.DismissEvent += (s2, arg2) => {
-                    Console.WriteLine("menu dismissed");
-                };
-
-                menu.Show();
+            // A menu item was clicked:
+            menu.MenuItemClick += (s1, arg1) => {
+                Console.WriteLine("{0} selected", arg1.Item!.TitleFormatted);
             };
-        }
+
+            // Menu was dismissed:
+            menu.DismissEvent += (s2, arg2) => {
+                Console.WriteLine("menu dismissed");
+            };
+
+            menu.Show();
+        };
     }
 }
