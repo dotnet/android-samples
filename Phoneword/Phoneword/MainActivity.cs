f248a71db5bcf0b357d65a36ef859705f6b3d6db
Leandra Hall <90208577+LeandraHall@users.noreply.github.com>
2022-07-12 15:30:01 -0700

    Phoneword Sample ported from Xamarin Forms to dotnet6

---
+++ Phoneword/Phoneword/MainActivity.cs
@@ -0,0 +1,41 @@
+using Core;
+
+namespace Phoneword;
+
+[Activity(Label = "Phone Word", MainLauncher = true, Icon = "@mipmap/ic_launcher")]
+public class MainActivity : Activity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.Main);
+
+        // Get our UI controls from the loaded layout
+        EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText)!;
+        Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton)!;
+        TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord)!;
+
+        // Add code to translate number
+        string translatedNumber = string.Empty;
+
+        translateButton.Click += (sender, e) =>
+        {
+            // Translate user’s alphanumeric phone number to numeric
+            translatedNumber = PhoneTranslator.ToNumber(phoneNumberText.Text);
+            
+            if (string.IsNullOrWhiteSpace(translatedNumber))
+            {
+                translatedPhoneWord.Text = string.Empty;
+            }
+            else
+            {
+                translatedPhoneWord.Text = translatedNumber;
+            }
+
+            ArgumentNullException.ThrowIfNull(translatedNumber);
+
+        };
+    }
+}
