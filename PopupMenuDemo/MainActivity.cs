47762174352767883e4588cdf50da72e62d8cbf7
Leandra Hall <90208577+LeandraHall@users.noreply.github.com>
2022-07-14 11:08:44 -0700

    Update MainActivity.cs to add indentation and var changes

--- PopupMenuDemo/MainActivity.cs
+++ PopupMenuDemo/MainActivity.cs
@@ -1,3 +1,4 @@
+
 namespace PopupMenuDemo;
 
 [Activity(Label = "PopupMenuDemo", MainLauncher = true)]
@@ -9,26 +10,26 @@ public class Activity1 : Activity
 
         SetContentView(Resource.Layout.Main);
 
-        Button showPopupMenu = FindViewById<Button>(Resource.Id.popupButton)!;
-            
+        var showPopupMenu = FindViewById<Button>(Resource.Id.popupButton)!;
+
         showPopupMenu!.Click += (s, arg) => {
 
-        PopupMenu menu = new PopupMenu(this, showPopupMenu);
+            var menu = new PopupMenu(this, showPopupMenu);
 
-        // Call inflate directly on the menu:
-        menu.Inflate(Resource.Menu.popup_menu);
+            // Call inflate directly on the menu:
+            menu.Inflate(Resource.Menu.popup_menu);
 
-        // A menu item was clicked:
-        menu.MenuItemClick += (s1, arg1) => {
-            Console.WriteLine("{0} selected", arg1.Item!.TitleFormatted);
-        };
+            // A menu item was clicked:
+            menu.MenuItemClick += (s1, arg1) => {
+                Console.WriteLine("{0} selected", arg1.Item!.TitleFormatted);
+            };
 
-        // Menu was dismissed:
-        menu.DismissEvent += (s2, arg2) => {
-            Console.WriteLine("menu dismissed");
-        };
+            // Menu was dismissed:
+            menu.DismissEvent += (s2, arg2) => {
+                Console.WriteLine("menu dismissed");
+            };
 
-        menu.Show();
+            menu.Show();
         };
     }
 }
