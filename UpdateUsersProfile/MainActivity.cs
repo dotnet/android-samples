0ce73e364b11f9d659a5e5203cb39daa6525572a
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-12-19 12:08:36 -0600

    Various fixes/cleanup
    * API 23 is min of sample
    * Update AndroidX, remove Android Support
    * Fix permission requests for latest android
    * General code cleanup, deleted commented code

--- UpdateUsersProfile/MainActivity.cs
+++ UpdateUsersProfile/MainActivity.cs
@@ -1,13 +1,9 @@
 using Android.Content;
-using Android.Provider;
-using Android.Database;
-using Android.OS;
-using AndroidX.Core.Content;
-using AndroidX.Core.App;
 using Android.Content.PM;
-using Android.Views;
-using static Android.App.ActionBar;
-using Android.Support.Design.Widget;
+using Android.Database;
+using Android.Provider;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
 
 namespace UpdateUsersProfile
 {
@@ -43,55 +39,35 @@ namespace UpdateUsersProfile
         // Give the device user a name: "John Doe"
         void NameOwner()
         {
-
             // Create the display name for the user's profile:
             var values = new ContentValues();
-            values.Put(ContactsContract.Contacts.InterfaceConsts.DisplayName, "John Doe");
+            values.Put(ContactsContract.Contacts.InterfaceConsts.DisplayName, "Jonathan Peppers");
 
             // Insert the user's name. Note that the user's profile entry cannot be created explicitly
             // (attempting to do so will throw an exception):
-
-            if (Android.OS.Build.VERSION.SdkInt > Android.OS.BuildVersionCodes.M)
-            {
-            if (ContextCompat.CheckSelfPermission(this, Android.Manifest.Permission.ReadContacts) == (int)Permission.Granted)
+            if (ContextCompat.CheckSelfPermission(this, Android.Manifest.Permission.ReadContacts) == Permission.Granted &&
+                ContextCompat.CheckSelfPermission(this, Android.Manifest.Permission.WriteContacts) == Permission.Granted)
             {
                 // We have permission, go ahead and access contacts
-                if (ContactsContract.Profile.ContentRawContactsUri is not null)
-                    ContentResolver?.Update(ContactsContract.Profile.ContentRawContactsUri, values, null, null);
-
+                ContentResolver?.Update(ContactsContract.Profile.ContentRawContactsUri!, values, null);
             }
             else
             {
                 // Contacts permission is not granted. Display rationale & request.
-
-                ActivityCompat.RequestPermissions(this, new String[] { Android.Manifest.Permission.Camera }, REQUEST_CONTACTS);
-
+                ActivityCompat.RequestPermissions(this, new String[] { Android.Manifest.Permission.ReadContacts, Android.Manifest.Permission.WriteContacts }, REQUEST_CONTACTS);
             }
-        }
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
         {
             if (requestCode == REQUEST_CONTACTS)
             {
-                // Received permission result for camera permission.
-                //Log.Info(TAG, "Received response for Location permission request.");
-
-                // Check if the only required permission has been granted
-                if ((grantResults.Length == 1) && (grantResults[0] == Permission.Granted))
+                // Check if the required permission(s) have been granted
+                if (grantResults.All(r => r == Permission.Granted))
                 {
-                    // Location permission has been granted, okay to retrieve the location of the device.
-                    //Log.Info(TAG, "Location permission has now been granted.");
                     NameOwner();
                 }
             }
-            //else
-            //{
-            //    if (Android.OS.Build.VERSION.SdkInt > Android.OS.BuildVersionCodes.M)
-            //    {
-            //        base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            //    }
-            //}
         }
 
         // Read back the user name and print it to the console:
@@ -99,7 +75,6 @@ namespace UpdateUsersProfile
         {
             // Get the URI for the user's profile:
             Android.Net.Uri? uri = ContactsContract.Profile.ContentUri;
-
             ArgumentNullException.ThrowIfNull(uri);
 
             // Setup the "projection" (columns we want) for only the display name:
@@ -107,24 +82,18 @@ namespace UpdateUsersProfile
 
             // Use a CursorLoader to retrieve the user's profile data:
             var loader = new AndroidX.Loader.Content.CursorLoader(this, uri, projection, null, null, null);
-
-
-            if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.M)
+            if (ContextCompat.CheckSelfPermission(this, Android.Manifest.Permission.ReadContacts) == Permission.Granted)
             {
-                if (ContextCompat.CheckSelfPermission(this, Android.Manifest.Permission.ReadContacts) == (int)Permission.Granted)
+                var cursor = (ICursor?)loader.LoadInBackground();
+
+                // Print the user name to the console if reading back succeeds:
+                if (cursor != null)
                 {
-                    var cursor = (ICursor?)loader.LoadInBackground();
-
-                    // Print the user name to the console if reading back succeeds:
-                    if (cursor != null)
+                    if (cursor.MoveToFirst())
                     {
-                        if (cursor.MoveToFirst())
-                        {
-                            Console.WriteLine(cursor.GetString(cursor.GetColumnIndex(projection[0])));
-                            return true;
-                        }
+                        Console.WriteLine(cursor.GetString(cursor.GetColumnIndex(projection[0])));
+                        return true;
                     }
-
                 }
             }
             return false;
@@ -133,7 +102,7 @@ namespace UpdateUsersProfile
         // Launch an intent that navigates to the user's profile:
         void ViewProfile()
         {
-            Intent intent = new Intent(Intent.ActionView, ContactsContract.Profile.ContentUri);
+            var intent = new Intent(Intent.ActionView, ContactsContract.Profile.ContentUri);
             StartActivity(intent);
         }
     }
