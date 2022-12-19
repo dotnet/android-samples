ba346015ae55810d3771224c6c36a7f660ae1491
Leandra Hall <90208577+LeandraHall@users.noreply.github.com>
2022-12-19 12:42:35 -0500

    Port Phoneword and PopupMenu samples (#340)
    * Pop Up Menu Sample ported from Xamarin to dotnet 6
    
    * Phoneword Sample ported from Xamarin to dotnet 6
    
    * Move screenshots up 1 directory
    
    We don't want them to show in the project
    
    * Line breaks
    
    * Better/faster TranslateToNumber
    
    * Update to .NET 7
    
    Co-authored-by: Jonathan Peppers <jonathan.peppers@microsoft.com>

---
+++ PopupMenuDemo/MainActivity.cs
@@ -0,0 +1,33 @@
+namespace PopupMenuDemo;
+
+[Activity(Label = "PopupMenuDemo", MainLauncher = true)]
+public class Activity1 : Activity
+{
+    protected override void OnCreate(Bundle? bundle)
+    {
+        base.OnCreate(bundle);
+
+        SetContentView(Resource.Layout.Main);
+
+        Button showPopupMenu = RequireViewById<Button>(Resource.Id.popupButton);
+        showPopupMenu.Click += (s, arg) => {
+
+            PopupMenu menu = new PopupMenu(this, showPopupMenu);
+
+            // Call inflate directly on the menu:
+            menu.Inflate(Resource.Menu.popup_menu);
+
+            // A menu item was clicked:
+            menu.MenuItemClick += (s1, arg1) => {
+                Console.WriteLine("{0} selected", arg1.Item!.TitleFormatted);
+            };
+
+            // Menu was dismissed:
+            menu.DismissEvent += (s2, arg2) => {
+                Console.WriteLine("menu dismissed");
+            };
+
+            menu.Show();
+        };
+    }
+}
