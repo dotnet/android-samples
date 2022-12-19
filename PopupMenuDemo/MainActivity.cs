20804b2d5195facb7458b492ded591acd8b57294
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-12-19 11:39:15 -0600

    Formatting

--- PopupMenuDemo/MainActivity.cs
+++ PopupMenuDemo/MainActivity.cs
@@ -13,22 +13,22 @@ public class Activity1 : Activity
             
         showPopupMenu!.Click += (s, arg) => {
 
-        PopupMenu menu = new PopupMenu(this, showPopupMenu);
+            PopupMenu menu = new PopupMenu(this, showPopupMenu);
 
-        // Call inflate directly on the menu:
-        menu.Inflate(Resource.Menu.popup_menu);
+            // Call inflate directly on the menu:
+            menu.Inflate(Resource.Menu.popup_menu);
 
-        // A menu item was clicked:
-        menu.MenuItemClick += (s1, arg1) => {
-            Console.WriteLine("{0} selected", arg1.Item!.TitleFormatted);
-        };
+            // A menu item was clicked:
+            menu.MenuItemClick += (s1, arg1) => {
+                Console.WriteLine("{0} selected", arg1.Item!.TitleFormatted);
+            };
 
-        // Menu was dismissed:
-        menu.DismissEvent += (s2, arg2) => {
-            Console.WriteLine("menu dismissed");
-        };
+            // Menu was dismissed:
+            menu.DismissEvent += (s2, arg2) => {
+                Console.WriteLine("menu dismissed");
+            };
 
-        menu.Show();
+            menu.Show();
         };
     }
 }
