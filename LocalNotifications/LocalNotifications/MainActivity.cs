f8f1934e8c2ff9a35f7a79a62e7ed8ac3d8c8e91
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-12-19 11:06:29 -0600

    Request appropriate permissions

--- LocalNotifications/LocalNotifications/MainActivity.cs
+++ LocalNotifications/LocalNotifications/MainActivity.cs
@@ -1,19 +1,25 @@
 using Android.Content;
+using Android.Content.PM;
 using Android.Content.Res;
+using Android.Runtime;
 using AndroidX.AppCompat.App;
 using AndroidX.Core.App;
 using Java.Lang;
-using TaskStackBuilder = Android.App.TaskStackBuilder;
+using TaskStackBuilder = AndroidX.Core.App.TaskStackBuilder;
 
 namespace LocalNotifications;
 
 [Activity (Label = "Notifications", MainLauncher = true, Icon = "@drawable/Icon")]
 public class MainActivity : AppCompatActivity
 {
-    // Unique ID for our notification: 
-    static readonly int NOTIFICATION_ID = 1000;
-    static readonly string CHANNEL_ID = "location_notification";
-    internal static readonly string COUNT_KEY = "count";
+    const string PERMISSION = "android.permission.POST_NOTIFICATIONS";
+    const int REQUEST_CODE = 1;
+    /// <summary>
+    /// Unique ID for our notification
+    /// </summary>
+    const int NOTIFICATION_ID = 1000;
+    const string CHANNEL_ID = "location_notification";
+    public const string COUNT_KEY = "count";
 
     // Number of times the button is tapped (starts with first tap):
     int count = 1;
@@ -23,48 +29,80 @@ public class MainActivity : AppCompatActivity
         base.OnCreate (bundle);
         SetContentView (Resource.Layout.Main);
 
-        CreateNotificationChannel ();
-
         // Display the "Hello World, Click Me!" button and register its event handler:
         var button = FindViewById<Button> (Resource.Id.MyButton);
         ArgumentNullException.ThrowIfNull (button);
-        button.Click += ButtonOnClick;
+        button.Click += (sender, e) =>
+        {
+            if (CheckSelfPermission(PERMISSION) == Permission.Denied)
+            {
+                ActivityCompat.RequestPermissions(this, new[] { PERMISSION }, REQUEST_CODE);
+            }
+            else
+            {
+                CreateNotification();
+            }
+        };
+
+        CreateNotificationChannel();
     }
 
-    // Handler for button click events.
-    void ButtonOnClick (object? sender, EventArgs eventArgs)
+    public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+    {
+        if (requestCode == REQUEST_CODE)
+        {
+            if (permissions.Length > 0 && permissions[0] == PERMISSION &&
+                grantResults.Length > 0 && grantResults[0] == Permission.Granted)
+            {
+                CreateNotification();
+            }
+            else
+            {
+                Toast.MakeText(this, "Unable to request permissions!", ToastLength.Long)!.Show();
+            }
+        }
+    }
+
+    void CreateNotification()
     {
         // Pass the current button press count value to the next activity:
-        var valuesForActivity = new Bundle ();
-        valuesForActivity.PutInt (COUNT_KEY, count);
+        var valuesForActivity = new Bundle();
+        valuesForActivity.PutInt(COUNT_KEY, count);
 
         // When the user clicks the notification, SecondActivity will start up.
-        var resultIntent = new Intent (this, typeof (SecondActivity));
+        var resultIntent = new Intent(this, typeof(SecondActivity));
 
         // Pass some values to SecondActivity:
-        resultIntent.PutExtras (valuesForActivity);
+        resultIntent.PutExtras(valuesForActivity);
 
         // Construct a back stack for cross-task navigation:
-        var stackBuilder = TaskStackBuilder.Create (this);
-        ArgumentNullException.ThrowIfNull (stackBuilder);
-        stackBuilder.AddParentStack (Class.FromType (typeof (SecondActivity)));
-        stackBuilder.AddNextIntent (resultIntent);
+        var stackBuilder = TaskStackBuilder.Create(this);
+        ArgumentNullException.ThrowIfNull(stackBuilder);
+        stackBuilder.AddParentStack(Class.FromType(typeof(SecondActivity)));
+        stackBuilder.AddNextIntent(resultIntent);
 
         // Create the PendingIntent with the back stack:            
-        var resultPendingIntent = stackBuilder.GetPendingIntent (0, PendingIntentFlags.Immutable);
+        var resultPendingIntent = stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.Immutable);
 
         // Build the notification:
-        var builder = new NotificationCompat.Builder (this, CHANNEL_ID)
-                        .SetAutoCancel (true) // Dismiss the notification from the notification area when the user clicks on it
-                        .SetContentIntent (resultPendingIntent) // Start up this activity when the user clicks the intent.
-                        .SetContentTitle ("Button Clicked") // Set the title
-                        .SetNumber (count) // Display the count in the Content Info
-                        .SetSmallIcon (Resource.Mipmap.ic_stat_button_click) // This is the icon to display
-                        .SetContentText ($"The button has been clicked {count} times."); // the message to display.
+        var builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .SetAutoCancel(true) // Dismiss the notification from the notification area when the user clicks on it
+                        .SetContentIntent(resultPendingIntent) // Start up this activity when the user clicks the intent.
+                        .SetContentTitle("Button Clicked") // Set the title
+                        .SetNumber(count) // Display the count in the Content Info
+                        .SetSmallIcon(Resource.Mipmap.ic_stat_button_click) // This is the icon to display
+                        .SetContentText($"The button has been clicked {count} times."); // the message to display.
 
         // Finally, publish the notification:
-        var notificationManager = NotificationManagerCompat.From (this);
-        notificationManager.Notify (NOTIFICATION_ID, builder.Build ());
+        var notificationManager = NotificationManagerCompat.From(this);
+        if (notificationManager.AreNotificationsEnabled())
+        {
+            notificationManager.Notify(NOTIFICATION_ID, builder.Build());
+        }
+        else
+        {
+            Toast.MakeText(this, "Notifications are not enabled!", ToastLength.Long)!.Show();
+        }
 
         // Increment the button press count:
         count++;
